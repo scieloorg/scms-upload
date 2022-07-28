e0f06421246c8933df9f02c9f9bf9241ec6b2e4c
Rafael JPD <rafael.pezzuto@gmail.com>
2022-07-28 14:57:18 -0300

    Cria rota para aceitar pacote

--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.utils.translation import gettext as _
 
-# Create your views here.
+from wagtail.admin import messages
+
+from .choices import PackageStatus
+from .models import Package
+
+
+def accept(request):
+    """
+    This view function abilitates the user to accept a package through the graphic-interface.
+
+    When a user clicks the "accept" button, the package state changes to "accept".
+    """
+    package_id = request.GET.get("package_id", None)
+    
+    if package_id:
+        package = get_object_or_404(Package, pk=package_id)
+
+    if request.method == 'GET':
+        package.status = PackageStatus.ACCEPTED
+        package.updated_by = request.user
+        package.save()
+        messages.success(request, _("Package was accepted."))
+
+    return redirect(request.META.get('HTTP_REFERER'))
