b5033ccf2584f4ca3098e3487ea78e82f6a97f36
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-01 22:10:24 -0300

    Remove import não utilizado

--- upload/wagtail_hooks.py
+++ upload/wagtail_hooks.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.urls import include, path
 from django.utils.translation import gettext as _
-from upload.choices import PackageStatus
 
 from wagtail.core import hooks
 from wagtail.contrib.modeladmin.options import (ModelAdmin, modeladmin_register)
