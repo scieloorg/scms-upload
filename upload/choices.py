50108dfc7f169d44475e0cb83e83fc0de3b0c458
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-02 14:45:43 -0300

    Substitui choices de status por uma lista simplificada

--- upload/choices.py
+++ upload/choices.py
@@ -4,10 +4,4 @@ from django.utils.translation import gettext as _
 
 class PackageStatus(models.TextChoices):
     SUBMITTED = 1, _('Submitted')
-    ACCEPTED = 2, _('Accepted')
-    ENQUEUED_FOR_VALIDATION = 3, _('Enqueued for validation')
-    VALIDATED_WITHOUT_ERRORS = 4, _('Validated without errors')
-    VALIDATED_WITH_ERRORS = 5, _('Validated with errors')
-    REJECTED = 6, _('Rejected')
-    SCHEDULED_FOR_PUBLICATION = 7, _('Scheduled for publication')
-    PUBLISHED = 8, _('Published')
+    FINISHED = 2, _('Finished')
