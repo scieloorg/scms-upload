46d2b22c70e9c65836240bf6dd276b010d7637d0
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-03 17:36:57 -0300

    Adiciona choices que representam severidade de erros de validação

--- upload/choices.py
+++ upload/choices.py
@@ -11,3 +11,12 @@ class PackageStatus(models.TextChoices):
     ACCEPTED = 6, _('Accepted')
     SCHEDULED_FOR_PUBLICATION = 7, _('Scheduled for publication')
     PUBLISHED = 8, _('Published')
+
+
+VALIDATION_ERROR_SEVERITY = [
+    ('criteria-issues', _('Criteria issues')),
+    ('warning', _('Warning')),
+    ('error', _('Error')),
+    ('fatal-error', _('Fatal error')),
+    ('blocking-error', _('Blocking error')),
+]
