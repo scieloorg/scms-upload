29fb3f8a576b94a3794418d3ba4149fe7361b0d7
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-03 12:50:16 -0300

    Compatibiliza status possíveis com o definido em projeto

--- upload/choices.py
+++ upload/choices.py
@@ -4,4 +4,10 @@ from django.utils.translation import gettext as _
 
 class PackageStatus(models.TextChoices):
     SUBMITTED = 1, _('Submitted')
-    FINISHED = 2, _('Finished')
+    ENQUEUED_FOR_VALIDATION = 2, _('Enqueued for validation')
+    VALIDATED_WITH_ERRORS = 3, _('Validated with errors')
+    VALIDATED_WITHOUT_ERRORS = 4, _('Validated without errors')
+    REJECTED = 5, _('Rejected')
+    ACCEPTED = 6, _('Accepted')
+    SCHEDULED_FOR_PUBLICATION = 7, _('Scheduled for publication')
+    PUBLISHED = 8, _('Published')
