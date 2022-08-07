6ab3a491fe2c85b2fda15e6449c91c18ec52ec24
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-07 14:05:57 -0300

    Renomeia campo snippet para message

--- upload/models.py
+++ upload/models.py
@@ -45,7 +45,7 @@ class ValidationError(models.Model):
 
     row = models.PositiveIntegerField(_('Row'), null=True, blank=True)
     column = models.PositiveIntegerField(_('Column'), null=True, blank=True)
-    snippet = models.TextField(_('Affected snippet'), max_length=255, null=True, blank=True)
+    message = models.CharField(_('Message'), max_length=128, null=True, blank=True)
 
     package = models.ForeignKey('Package', on_delete=models.CASCADE, null=False, blank=False)
 
