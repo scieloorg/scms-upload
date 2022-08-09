45a57033a3b77e1e623607de30034b0954fcaeef
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-09 15:59:00 -0300

    Adiciona método utilitário para obter trecho de XML afetado por erro

--- upload/utils/xml_utils.py
+++ upload/utils/xml_utils.py
@@ -28,3 +28,19 @@ def convert_xml_str_to_etree(xml_str):
             column=col,
             message=msg,
         )
+
+
+def get_snippet(xml_data, start_row, end_row):
+    lines = []
+
+    if not start_row:
+        return lines
+
+    if not end_row:
+        end_row = start_row
+
+    for line_number, content in numbered_lines(xml_data):
+        if line_number >= start_row and line_number <= end_row:
+            lines.append(content.encode())
+
+    return lines
