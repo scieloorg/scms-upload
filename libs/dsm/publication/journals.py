f3855c65d006dc2ffa4c546ac4313a415dc7f74f
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-08-10 11:18:20 -0300

    Refatora get_journal

--- libs/dsm/publication/journals.py
+++ libs/dsm/publication/journals.py
@@ -8,29 +8,25 @@ from opac_schema.v1.models import (
     LastIssue,
 )
 
-from libs.dsm.publication import db
 
-
-def get_journal(scielo_issn):
+def get_journal(journal_id):
     """
     Get registered journal or new journal
 
     Parameters
     ----------
-    scielo_issn : str
+    journal_id : str
 
-    Raises
-    ------
-    exceptions.RecordNotFoundError
 
     Returns
     -------
     Journal
     """
-    journal = db.fetch_record(scielo_issn, Journal)
-    if not journal:
+    try:
+        journal = Journal.objects.get(_id=journal_id)
+    except Journal.DoesNotExist:
         journal = Journal()
-        journal._id = scielo_issn
+        journal._id = journal_id
     return journal
 
 
