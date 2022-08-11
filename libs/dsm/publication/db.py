7ca06d0358f56a77eeff4f66ba65235aca8fd373
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-08-11 19:57:40 -0300

    Cria função para conectar à base de dados

--- libs/dsm/publication/db.py
+++ libs/dsm/publication/db.py
@@ -1,6 +1,37 @@
 from datetime import datetime
 
+from tenacity import (
+    retry,
+    wait_exponential,
+    stop_after_attempt,
+)
+from mongoengine import (
+    connect,
+)
+
 from . import exceptions
+
+
+def mk_connection(host, alias=None):
+    try:
+        return _db_connect_by_uri(host, alias)
+    except Exception as e:
+        raise exceptions.DBConnectError(
+            str({"exception": type(e), "msg": str(e)})
+        )
+
+
+@retry(wait=wait_exponential(), stop=stop_after_attempt(10))
+def _db_connect_by_uri(uri, alias=None):
+    """
+    mongodb://{login}:{password}@{host}:{port}/{database}
+    """
+    params = {"host": uri, "maxPoolSize": None}
+    if alias:
+        params['alias'] = alias
+    conn = connect(**params)
+    print("%s connected" % params)
+    return conn
 
 
 def fetch_record(_id, model, **kwargs):
--- libs/dsm/publication/exceptions.py
+++ libs/dsm/publication/exceptions.py
@@ -1,3 +1,7 @@
+class DBConnectError(Exception):
+    ...
+
+
 class FetchRecordError(Exception):
     ...
 
