bac87e78282289c5c37359a83409d86f059cc9b8
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-08-17 17:23:24 -0300

    Cria função para publicar dados de documentos

--- libs/dsm/publication/documents.py
+++ libs/dsm/publication/documents.py
@@ -39,6 +39,46 @@ def publish_document(doc_data):
         doc = get_document(doc_data["id"])
         doc.journal = Journal.objects.get(_id=doc_data["journal_id"])
         doc.issue = Issue.objects.get(_id=doc_data["issue_id"])
+        doc.issue.is_public = True
+
+        # atualiza status
+        doc.is_public = True
+
+        # Dados principais (versão considerada principal)
+        # devem conter estilos html (math, italic, sup, sub)
+        doc.title = doc_data["title"]
+        doc.section = doc_data["section"]
+        doc.abstract = doc_data["abstract"]
+        doc.original_language = doc_data["language"]
+        doc.doi = doc_data.get("doi")
+
+        # Identificadores
+        doc._id = doc_data["id"]
+        doc.aid = doc_data["id"]
+        doc.pid = doc_data["v2"]
+
+        doc.scielo_pids = {}
+        doc.scielo_pids["v2"] = doc_data["v2"]
+        doc.scielo_pids["v3"] = doc_data["v3"]
+
+        if doc_data.get("other"):
+            doc.scielo_pids["other"] = doc_data.get("other_pids")
+
+        if doc_data.get("aop_pid"):
+            doc.aop_pid = doc_data.get("aop_pid")
+
+        doc.publication_date = doc_data["publication_date"]
+        doc.type = doc_data["publication_type"]
+
+        # Dados de localização no issue
+        doc.elocation = doc_data["elocation"]
+        doc.fpage = doc_data["fpage"]
+        doc.fpage_sequence = doc_data["fpage_seq"]
+        doc.lpage = doc_data["lpage"]
+
+        doc.order = doc_data["order"]
+
+        doc.xml = doc_data["xml"]
 
     except KeyError as e:
         raise exceptions.DocumentDataError(e)
@@ -49,6 +89,3 @@ def publish_document(doc_data):
         raise exceptions.DocumentSaveError(e)
 
     return doc
-
-
-
