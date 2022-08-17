4926d6a2dad745790074533ef3215858d0a997e9
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-16 21:59:47 -0300

    Cria validador de assets

--- upload/tasks.py
+++ upload/tasks.py
@@ -11,15 +11,20 @@ from . import choices, controller
 User = get_user_model()
 
 
-def validate_xml_format(filename, package_id):
-    return task_validate_xml_format.delay(filename, package_id)
+def run_validations(filename, package_id):
+    xml_format_is_valid = task_validate_xml_format(filename, package_id)
+
+    if xml_format_is_valid:
+        task_validate_assets.delay(filename, package_id)
+        task_validate_renditions.delay(filename, package_id)    
 
 
-@celery_app.task(bind=True,  max_retries=3)
-def task_validate_xml_format(self, file_path, package_id):    
+@celery_app.task()
+def task_validate_xml_format(file_path, package_id):    
     try:
         xml_str = file_utils.get_xml_content_from_zip(file_path)
-        xml_utils.convert_xml_str_to_etree(xml_str)
+        _ = xml_utils.convert_xml_str_to_etree(xml_str)
+        return True
 
     except (file_utils.BadPackageFileError, file_utils.PackageWithoutXMLFileError):
         controller.add_validation_error(
@@ -44,4 +49,52 @@ def task_validate_xml_format(self, file_path, package_id):
         )
 
     return False
+
+
+@celery_app.task()
+def task_validate_assets(file_path, package_id):
+    source = file_utils.get_file_absolute_path(file_path)
+    target = file_utils.generate_optimized_filepath(source)
+    package_utils.optimise_package(source, target)
+    package_utils.unzip(target)
+
+    package_files = file_utils.get_file_list_from_zip(target)
+
+    for a in package_utils.evaluate_assets(target, package_files):
+        if not a['is_present']:
+            controller.add_validation_error(
+                choices.VE_ASSET_ERROR,
+                package_id,
+                choices.PS_REJECTED,
+                message=f'{a["name"]} {_("file is mentioned in the XML but not present in the package.")}',
+                data={
+                    'id': a['id'],
+                    'type': a['type'],
+                    'missing_file': a['name'],
+                },
+            )
+
+            controller.add_validation_error(
+                choices.VE_ASSET_ERROR,
+                package_id,
+                choices.PS_REJECTED,
+                message=f'{a["name"]} {_("file is mentioned in the XML but its optimised version not present in the package.")}',
+                data={
+                    'id': a['id'],
+                    'type': _('Optimised'),
+                    'missing_file': a['name'].replace('.tif', '.png'),
+                },
+            )
+
+            controller.add_validation_error(
+                choices.VE_ASSET_ERROR,
+                package_id,
+                choices.PS_REJECTED,
+                message=f'{a["name"]} {_("file is mentioned in the XML but its thumbnail version not present in the package.")}',
+                data={
+                    'id': a['id'],
+                    'type': _('Thumbnail'),
+                    'missing_file': a['name'].replace('.tif', '.thumbnail.jpg'),
+                },
+            )
 
