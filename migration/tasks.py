7013cbc23ab1e9c7737e6810faebadfbd3cf5cbc
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-08-19 14:00:48 -0300

    Cria task para migrar e publicar dados de fascículos

--- migration/tasks.py
+++ migration/tasks.py
@@ -31,3 +31,26 @@ def task_migrate_journal(self, pid, data):
             controller.PublishJournalError,
             ) as e:
         logging.error(e)
+
+
+def migrate_issues(source_file_path, connection):
+    controller.connect(connection)
+    for pid, data in controller.get_classic_website_records("issue", source_file_path):
+        task_migrate_issue.delay(pid, data)
+
+
+@celery_app.task(bind=True, max_retries=3)
+def task_migrate_issue(self, pid, data):
+    try:
+        controller.migrate_issue(pid, data)
+    except (
+            controller.MigratedIssueSaveError,
+            controller.IssueMigrationTrackSaveError,
+            ) as e:
+        logging.error(e)
+    try:
+        controller.publish_issue(pid)
+    except (
+            controller.PublishIssueError,
+            ) as e:
+        logging.error(e)
