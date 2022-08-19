e9b6207c120746ea83ce84d46a837769c3676a62
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-08-19 13:52:30 -0300

    Cria os modelos para registrar a migração de issue e os dados migrados de issues

--- migration/models.py
+++ migration/models.py
@@ -49,3 +49,46 @@ class MigratedJournal(CommonControlField):
 
     def __str__(self):
         return f"{self.acron} {self.scielo_issn} {self.title}"
+
+
+class IssueMigrationTracker(CommonControlField):
+
+    scielo_issn = models.CharField(
+        _('SciELO ISSN'), max_length=9, null=False, blank=False)
+    acron = models.CharField(
+        _('Acronym'), max_length=20, null=False, blank=False)
+    issue_pid = models.CharField(
+        _('Issue PID'), max_length=9, null=False, blank=False)
+    year = models.CharField(
+        _('Publication Year'), max_length=4, null=False, blank=False
+    )
+
+    # datas no registro da base isis para identificar
+    # se houve mudança durante a migração
+    isis_updated_date = models.CharField(
+        _('ISIS updated date'), max_length=8, null=False, blank=False)
+    isis_created_date = models.CharField(
+        _('ISIS created date'), max_length=8, null=False, blank=False)
+
+    # status do registro quanto aos metadados
+    status = models.CharField(
+        _('Status'), max_length=20,
+        choices=choices.MIGRATION_STATUS,
+        default=choices.MS_TO_MIGRATE,
+    )
+
+    migrated_issue = models.ForeignKey('MigratedIssue')
+
+    def __str__(self):
+        return f"{self.acron} {self.issue_pid} {self.status}"
+
+
+class MigratedIssue(CommonControlField):
+
+    issue_pid = models.CharField(
+        _('Issue PID'), max_length=9, null=False, blank=False)
+    # registro no formato json correspondente ao conteúdo da base isis
+    record = models.JSONField(blank=False)
+
+    def __str__(self):
+        return f"{self.issue_pid}"
