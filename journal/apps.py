ff8e7ee6ec8fbe703701cc460ca38b95aaa4d543
Edgar <eduranm@dgb.unam.mx>
2022-08-23 12:22:27 -0500

    Agrega modelo para Non Official Journal Titles

--- journal/apps.py
+++ journal/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JournalsConfig(AppConfig):
+class JournalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'journals'
+    name = 'journal'
--- journal/models.py
+++ journal/models.py
@@ -3,13 +3,16 @@ from django.utils.translation import gettext_lazy as _
 
 from core.models import CommonControlField
 from .forms import OfficialJournalForm
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.core.models import Orderable
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
 
 
 class OfficialJournal(CommonControlField):
     """
     Class that represent the Official Journal
     """
-
     title = models.CharField(_('Official Title'), max_length=256, null=True, blank=True)
     foundation_year = models.CharField(_('Foundation Year'), max_length=4, null=True, blank=True)
     ISSN_print = models.CharField(_('ISSN Print'), max_length=9, null=True, blank=True)
@@ -17,3 +20,20 @@ class OfficialJournal(CommonControlField):
     ISSNL = models.CharField(_('ISSNL'), max_length=9, null=True, blank=True)
 
     base_form_class = OfficialJournalForm
+
+
+class NonOfficialJournalTitle(ClusterableModel, CommonControlField):
+
+    official_journal_id = models.ForeignKey('OfficialJournal', null=True, blank=False, related_name='OfficialJournal', on_delete=models.SET_NULL)
+
+    panels=[
+        FieldPanel('official_journal_id'),
+        InlinePanel('page_non_official_title', label=_('Non Official Journal Title'))
+    ]
+
+    base_form_class = OfficialJournalForm
+
+
+class NonOfficialTitle(Orderable):
+   page = ParentalKey(NonOfficialJournalTitle, related_name='page_non_official_title')
+   non_official_journal_title = models.CharField(_('Non Official Journal Title'),max_length=255, null=False, blank=False)
