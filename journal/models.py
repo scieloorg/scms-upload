6753bfe147af5fcf67ee00b52a18b18ed7d98f20
Edgar <eduranm@dgb.unam.mx>
2022-08-23 10:19:49 -0500

    Cambio de nombre a carpeta Journal

---
+++ journal/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from core.models import CommonControlField
+from .forms import OfficialJournalForm
+
+
+class OfficialJournal(CommonControlField):
+    """
+    Class that represent the Official Journal
+    """
+
+    title = models.CharField(_('Official Title'), max_length=256, null=True, blank=True)
+    foundation_year = models.CharField(_('Foundation Year'), max_length=4, null=True, blank=True)
+    ISSN_print = models.CharField(_('ISSN Print'), max_length=9, null=True, blank=True)
+    ISSN_electronic = models.CharField(_('ISSN Eletronic'), max_length=9, null=True, blank=True)
+    ISSNL = models.CharField(_('ISSNL'), max_length=9, null=True, blank=True)
+
+    base_form_class = OfficialJournalForm
