cfdd13535756c19a47eef4701497a510e35e4ffa
Edgar <eduranm@dgb.unam.mx>
2022-08-24 11:12:27 -0500

    Agrega modelo NonOfficialJournalTitle

--- journal/models.py
+++ journal/models.py
@@ -3,6 +3,12 @@ from django.utils.translation import gettext_lazy as _
 
 from core.models import CommonControlField
 from .forms import OfficialJournalForm
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, PageChooserPanel
+from wagtail.core.models import Orderable
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+from django import forms
+from wagtail.admin import widgets 
 
 
 class OfficialJournal(CommonControlField):
@@ -17,3 +23,20 @@ class OfficialJournal(CommonControlField):
     ISSNL = models.CharField(_('ISSNL'), max_length=9, null=True, blank=True)
 
     base_form_class = OfficialJournalForm
+
+
+class NonOfficialJournalTitle(ClusterableModel, CommonControlField):
+
+    official_journal_id = models.ForeignKey('OfficialJournal', null=False, blank=False, related_name='OfficialJournal', on_delete=models.CASCADE)
+
+    panels=[
+        FieldPanel('official_journal_id'),
+        InlinePanel('page_non_official_title', label=_('Non Official Journal Title'))
+    ]
+
+    base_form_class = OfficialJournalForm
+
+
+class NonOfficialTitle(Orderable):
+   page = ParentalKey(NonOfficialJournalTitle, related_name='page_non_official_title')
+   non_official_journal_title = models.CharField(_('Non Official Journal Title'),max_length=255, null=False, blank=False)
