80a192973e34fb85dfae099c4c81ed43f9496527
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-25 21:38:38 -0300

    Cria rotas que respondem por objeto ErrorResolution

--- upload/urls.py
+++ upload/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import (finish_deposit, preview_document, validation_report)
+from .views import (error_resolution, ajx_error_resolution, finish_deposit, preview_document, validation_report)
 
 
 app_name = "upload"
 
 urlpatterns = [
+    path("ajx-error-resolution/", view=ajx_error_resolution, name="ajx_error_resolution"),
+    path("error-resolution", view=error_resolution, name="error_resolution"),
     path("preview-document", view=preview_document, name="preview_document"),
     path("validation-report", view=validation_report, name="validation_report"),
     path("finish", view=finish_deposit, name="finish_deposit"),
