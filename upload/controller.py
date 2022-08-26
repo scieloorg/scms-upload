866bfd660cd2f7c3f059dc773951c1fd05d51423
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-25 21:37:21 -0300

    Adiciona método para criar ou atualizar ErrorResolution

--- upload/controller.py
+++ upload/controller.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from django.shortcuts import get_object_or_404
 
-from .models import Package, ValidationError
+from .models import ErrorResolution, Package, ValidationError, choices
 
 
 def add_validation_error(error_category, package_id, package_status, message=None, data=None):
@@ -14,3 +16,25 @@ def add_validation_error(error_category, package_id, package_status, message=Non
 
     ve.package.save()
     ve.save()
+
+
+def upsert_validation_error_resolution(validation_error_id, user, action, comment):
+    ve = ValidationError.objects.get(pk=validation_error_id)
+
+    try:
+        er = ErrorResolution.objects.get(pk=validation_error_id)
+        er.updated = datetime.now()
+        er.updated_by = user
+    except ErrorResolution.DoesNotExist:
+        er = ErrorResolution()
+        er.creator = user
+        er.created = datetime.now()
+
+    er.action = action
+    er.comment = comment
+    er.validation_error = ve
+    
+    er.save()
+
+    return er
+
