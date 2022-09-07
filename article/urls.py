fd0de19b06a95f53e5a20871534aa959e7108460
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-07 18:39:51 -0300

    Criação de rota request-change

--- article/urls.py
+++ article/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path
+
+from .views import request_change
+
+
 app_name = "article"
 
-urlpatterns = []
+urlpatterns = [
+    path("request-change", view=request_change, name="request_change"),
+]
