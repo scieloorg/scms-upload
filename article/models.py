841507247845e66a1682dc81a3dff7a48596e490
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-07 15:33:28 -0300

    Adiciona pub_year e outros campos

--- article/models.py
+++ article/models.py
@@ -16,3 +16,16 @@ class Article(ClusterableModel, CommonControlField):
     pid_v3 = models.CharField(_('PID v3'), max_length=23, blank=True, null=True)
     pid_v2 = models.CharField(_('PID v2'), primary_key=True, max_length=23, blank=False, null=False)
     aop_pid = models.CharField(_('AOP PID'), max_length=23, blank=True, null=True)
+
+    # Year of publication
+    pub_year = models.IntegerField(_('Publication year'), blank=True, null=True)
+
+    # Issue
+    volume = models.CharField(_('Volume'), max_length=32, blank=True, null=True)
+    number = models.CharField(_('Number'), max_length=16, blank=True, null=True)
+    suppl = models.CharField(_('Supplement'), max_length=32, blank=True, null=True)
+
+    # Page
+    elocation_id = models.CharField(_('Elocation ID'), max_length=64, blank=True, null=True)
+    fpage = models.CharField(_('First page'), max_length=16, blank=True, null=True)
+    lpage = models.CharField(_('Last page'), max_length=16, blank=True, null=True)
