19f69a250c831801f992a9a9676f7a4bf5470aff
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-07 15:36:58 -0300

    Cria classes relacionados a DoiWithLang

--- article/models.py
+++ article/models.py
@@ -95,3 +95,24 @@ class Author(models.Model):
 class ArticleAuthor(Orderable, Author):
     author = ParentalKey('Article', on_delete=models.CASCADE, related_name='author')
 
+
+class DOIWithLang(models.Model):
+    doi = models.CharField(_('DOI'), max_length=255, blank=False, null=False)
+    lang = models.CharField(_('Language'), max_length=64, blank=False, null=False)
+
+    panels = [
+        FieldPanel('doi'),
+        FieldPanel('lang'),
+    ]
+
+    def __str__(self):
+        return f'{self.lang.upper()}: {self.doi}'
+
+    class Meta:
+        abstract = True
+
+
+class ArticleDOIWithLang(Orderable, DOIWithLang):
+    doi_with_lang = ParentalKey('Article', on_delete=models.CASCADE, related_name='doi_with_lang')
+
+
