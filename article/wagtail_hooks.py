cb934c3269f0f2d8413c0c8c278c4b99895d1d41
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-07 15:39:31 -0300

    Configura quais campos serão associados às diversas views

--- article/wagtail_hooks.py
+++ article/wagtail_hooks.py
@@ -28,3 +28,46 @@ class ArticleModelAdmin(ModelAdmin):
     def doi_list(self, obj):
         return ', '.join([str(dl) for dl in obj.doi_with_lang.all()])
 
+    list_display = (
+        'pid_v2',
+        'pid_v3',
+        'doi_list',
+        'aop_pid',
+        'journal',
+        'created',
+        'updated',
+        'updated_by',
+    )
+    list_filter = (
+        'journal',
+    )
+    search_fields = (
+        'pid_v2',
+        'pid_v3',
+        'doi_with_lang__doi',
+        'aop_pid',
+        'journal__title',
+        'journal__ISSNL',
+    )
+    inspect_view_fields = (
+        'created',
+        'updated',
+        'creator',
+        'updated_by',
+        'pid_v3',
+        'pid_v2',
+        'doi_with_lang',
+        'aop_pid',
+        'related_item',
+        'author',
+        'title_with_lang',
+        'pub_year',
+        'volume',
+        'number',
+        'suppl',
+        'elocation_id',
+        'fpage',
+        'lpage',
+        'journal',
+    )
+
