088068dc59a64eea9425043555eda08a307fa3de
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-07 15:31:13 -0300

    Cria módulo models.py para app article e faz imports

---
+++ article/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import InlinePanel, FieldPanel, MultiFieldPanel
+from wagtail.core.models import Orderable, ClusterableModel
+
+from core.models import CommonControlField
+from journal.models import OfficialJournal
+
+from .forms import ArticleForm
