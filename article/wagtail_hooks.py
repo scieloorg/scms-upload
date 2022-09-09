a0fb1573e84024484283e88cadf7636d7babde1a
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-09 12:51:51 -0300

    Faz imports

--- article/wagtail_hooks.py
+++ article/wagtail_hooks.py
@@ -3,10 +3,10 @@ from django.urls import include, path
 from django.utils.translation import gettext as _
 
 from wagtail.core import hooks
-from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from wagtail.contrib.modeladmin.options import ModelAdmin, ModelAdminGroup, modeladmin_register
 from wagtail.contrib.modeladmin.views import CreateView
 
-from .models import Article
+from .models import Article, RelatedItem
 
 
 class ArticleCreateView(CreateView):
