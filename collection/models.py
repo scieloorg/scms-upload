fedba31fe9b159c0f37a50a060a12bba532f77c4
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-09-24 19:56:26 -0300

    Faz ajustes em atributos e adiciona índices para agilizar as buscas

--- collection/models.py
+++ collection/models.py
@@ -6,6 +6,8 @@ from journal.models import OfficialJournal
 from issue.models import Issue
 from article.models import Article
 
+from .choices import JOURNAL_PUBLICATION_STATUS, WEBSITE_KIND
+
 
 class Collection(CommonControlField):
     """
@@ -18,7 +20,7 @@ class Collection(CommonControlField):
     def __str__(self):
         return u'%s %s' % (self.name, self.acron)
 
-    acron = models.CharField(_('Collection Acronym'), max_length=255, null=False, blank=False)
+    acron = models.CharField(_('Collection Acronym'), max_length=255, null=True, blank=True)
     name = models.CharField(_('Collection Name'), max_length=255, null=True, blank=True)
 
 
@@ -26,25 +28,36 @@ class SciELOJournal(CommonControlField):
     """
     Class that represent the Journal in a Collection
     """
-
-    # TODO futuramente ter um formulário para gerir os dados
-
-    def __unicode__(self):
-        return u'%s %s %s' % (self.collection, self.scielo_issn, self.acron)
-
-    def __str__(self):
-        return u'%s %s %s' % (self.collection, self.scielo_issn, self.acron)
-
-    # ID na coleção
     scielo_issn = models.CharField(_('SciELO ISSN'), max_length=9, null=False, blank=False)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
     acron = models.CharField(_('Acronym'), max_length=25, null=True, blank=True)
     title = models.CharField(_('Title'), max_length=255, null=True, blank=True)
+    publication_status = models.CharField(
+        _('Publication Status'), max_length=10, null=True, blank=True,
+        choices=JOURNAL_PUBLICATION_STATUS)
 
     # TODO acrescentar
     # data de entrada
     # data de saída
     # motivo da saída
+
+    class Meta:
+        unique_together = [
+            ['collection', 'scielo_issn'],
+            ['collection', 'acron'],
+        ]
+        indexes = [
+            models.Index(fields=['acron']),
+            models.Index(fields=['collection']),
+            models.Index(fields=['scielo_issn']),
+            models.Index(fields=['publication_status']),
+        ]
+
+    def __unicode__(self):
+        return u'%s %s' % (self.collection, self.scielo_issn)
+
+    def __str__(self):
+        return u'%s %s' % (self.collection, self.scielo_issn)
 
 
 class JournalCollections(CommonControlField):
@@ -53,13 +66,19 @@ class JournalCollections(CommonControlField):
     """
 
     def __unicode__(self):
-        return u'%s %s' % (self.official_journal, " | ".join([str(item) for item in self.scielo_journals]))
+        return u'%s %s' % (self.official_issue, " | ".join([str(item) for item in self.scielo_journals]))
 
     def __str__(self):
-        return u'%s %s' % (self.official_journal, " | ".join([str(item) for item in self.scielo_journals]))
+        return u'%s %s' % (self.official_issue, " | ".join([str(item) for item in self.scielo_journals]))
 
     official_journal = models.ForeignKey(OfficialJournal, on_delete=models.CASCADE)
     scielo_journals = models.ManyToManyField(SciELOJournal)
+
+    class Meta:
+
+        indexes = [
+            models.Index(fields=['official_journal']),
+        ]
 
 
 class SciELOIssue(CommonControlField):
@@ -68,15 +87,29 @@ class SciELOIssue(CommonControlField):
     """
 
     def __unicode__(self):
-        return u'%s %s %s' % (self.scielo_journal, self.issue_pid, self.issue_folder)
+        return u'%s %s' % (self.scielo_journal, self.issue_pid)
 
     def __str__(self):
-        return u'%s %s %s' % (self.scielo_journal, self.issue_pid, self.issue_folder)
+        return u'%s %s' % (self.scielo_journal, self.issue_pid)
 
     scielo_journal = models.ForeignKey(SciELOJournal, on_delete=models.CASCADE)
     issue_pid = models.CharField(_('Issue PID'), max_length=17, null=False, blank=False)
     # v30n1 ou 2019nahead
     issue_folder = models.CharField(_('Issue Folder'), max_length=17, null=False, blank=False)
+    pub_year = models.CharField(_('Publicatin year'), max_length=4, null=True, blank=True)
+
+    class Meta:
+        unique_together = [
+            ['scielo_journal', 'issue_pid'],
+            ['scielo_journal', 'issue_folder'],
+            ['issue_pid', 'issue_folder'],
+        ]
+        indexes = [
+            models.Index(fields=['scielo_journal']),
+            models.Index(fields=['issue_pid']),
+            models.Index(fields=['issue_folder']),
+            models.Index(fields=['pub_year']),
+        ]
 
 
 class IssueInCollections(CommonControlField):
@@ -93,6 +126,11 @@ class IssueInCollections(CommonControlField):
     official_issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
     scielo_issues = models.ManyToManyField(SciELOIssue)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=['official_issue']),
+        ]
+
 
 class SciELODocument(CommonControlField):
     """
@@ -100,14 +138,26 @@ class SciELODocument(CommonControlField):
     """
 
     def __unicode__(self):
-        return u'%s %s %s' % (self.scielo_issue, self.pid, self.file_id)
+        return u'%s %s' % (self.scielo_issue, self.pid)
 
     def __str__(self):
-        return u'%s %s %s' % (self.scielo_issue, self.pid, self.file_id)
+        return u'%s %s' % (self.scielo_issue, self.pid)
 
     scielo_issue = models.ForeignKey(SciELOIssue, on_delete=models.CASCADE)
-    pid = models.CharField(_('PID'), max_length=17, null=True, blank=True)
+    pid = models.CharField(_('PID'), max_length=23, null=True, blank=True)
     file_id = models.CharField(_('File ID'), max_length=17, null=True, blank=True)
+
+    class Meta:
+        unique_together = [
+            ['scielo_issue', 'pid'],
+            ['scielo_issue', 'file_id'],
+            ['pid', 'file_id'],
+        ]
+        indexes = [
+            models.Index(fields=['scielo_issue']),
+            models.Index(fields=['pid']),
+            models.Index(fields=['file_id']),
+        ]
 
 
 class DocumentInCollections(CommonControlField):
@@ -116,10 +166,15 @@ class DocumentInCollections(CommonControlField):
     """
 
     def __unicode__(self):
-        return u'%s %s' % ('self.offical_doc', " | ".join([str(item) for item in self.scielo_docs]))
+        return u'%s %s' % (self.official_document, " | ".join([str(item) for item in self.scielo_documents]))
 
     def __str__(self):
-        return u'%s %s' % ('self.offical_doc', " | ".join([str(item) for item in self.scielo_docs]))
+        return u'%s %s' % (self.official_document, " | ".join([str(item) for item in self.scielo_documents]))
 
-    official_doc = models.ForeignKey(Article, on_delete=models.CASCADE)
-    scielo_docs = models.ManyToManyField(SciELODocument)
+    official_document = models.ForeignKey(Article, on_delete=models.CASCADE, null=True, blank=True)
+    scielo_documents = models.ManyToManyField(SciELODocument)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['official_document']),
+        ]
