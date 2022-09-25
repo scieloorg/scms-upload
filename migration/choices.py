4377307608d8e03609bc5734309864bdb2797c16
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-09-25 18:06:25 -0300

    Adiciona novos para valores para status da publicação e adiciona exceptions

--- migration/choices.py
+++ migration/choices.py
@@ -1,12 +1,14 @@
 from django.utils.translation import gettext as _
 
 
+MS_TO_IGNORE = 'TO_IGNORE'
 MS_TO_MIGRATE = 'TO_MIGRATE'
 MS_MIGRATED = 'MIGRATED'
 MS_PUBLISHED = 'PUBLISHED'
 
 MIGRATION_STATUS = (
     (MS_TO_MIGRATE, _('To migrate')),
+    (MS_TO_IGNORE, _('To ignore')),
     (MS_MIGRATED, _('Migrated')),
     (MS_PUBLISHED, _('Published')),
 )
---
+++ migration/exceptions.py
@@ -0,0 +1,86 @@
+class DocumentFilesMigrationSaveError(Exception):
+    ...
+
+
+class DocumentMigrationSaveError(Exception):
+    ...
+
+
+class DocumentPublicationForbiddenError(Exception):
+    ...
+
+
+class GetFilesStorageError(Exception):
+    ...
+
+
+class GetOrCreateDocumentFilesMigrationError(Exception):
+    ...
+
+
+class GetOrCreateDocumentMigrationError(Exception):
+    ...
+
+
+class GetOrCreateIssueFilesMigrationError(Exception):
+    ...
+
+
+class GetOrCreateIssueMigrationError(Exception):
+    ...
+
+
+class GetOrCreateJournalMigrationError(Exception):
+    ...
+
+
+class IssueFilesMigrationGetError(Exception):
+    ...
+
+
+class IssueFilesMigrationSaveError(Exception):
+    ...
+
+
+class IssueFilesStoreError(Exception):
+    ...
+
+
+class IssueMigrationSaveError(Exception):
+    ...
+
+
+class IssuePublicationForbiddenError(Exception):
+    ...
+
+
+class JournalMigrationSaveError(Exception):
+    ...
+
+
+class JournalPublicationForbiddenError(Exception):
+    ...
+
+
+class PublishDocumentError(Exception):
+    ...
+
+
+class PublishIssueError(Exception):
+    ...
+
+
+class PublishJournalError(Exception):
+    ...
+
+
+class GetMigrationConfigurationError(Exception):
+    ...
+
+
+class GetOrCreateCrontabScheduleError(Exception):
+    ...
+
+
+class GetJournalMigratioStatusError(Exception):
+    ...
