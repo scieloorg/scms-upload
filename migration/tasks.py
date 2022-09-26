45dc58c407e1f6f1512a8906d94aec1e993a5b9a
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-09-25 22:15:24 -0300

    Adiciona a task para migrar e publicar dados de periódico

---
+++ migration/tasks.py
@@ -0,0 +1,22 @@
+import logging
+
+from django.utils.translation import gettext_lazy as _
+
+from config import celery_app
+from celery.exceptions import SoftTimeLimitExceeded
+
+from . import controller
+
+
+@celery_app.task(bind=True, name=_('Migrate and publish journals'))
+def task_migrate_and_publish_journals(
+        self,
+        user_id,
+        collection_acron,
+        force_update=False,
+        ):
+    controller.migrate_and_publish_journals(
+        user_id,
+        collection_acron,
+        force_update,
+    )
