50bea5d94dae97d389d9b74700ac15184eb7f853
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-26 14:37:46 -0300

    Faz ajustes finais (imports faltantes, etc)

--- upload/controller.py
+++ upload/controller.py
@@ -90,12 +90,13 @@ def update_package_check_opinions(package_id):
     package.save()
 
 
-def create_package(article_id, user_id, file_name, status=choices.PS_PUBLISHED):
+def create_package(article_id, user_id, file_name, category=choices.PC_SYSTEM_GENERATED, status=choices.PS_PUBLISHED):
     package = Package()
     package.article_id = article_id
     package.creator_id = user_id
     package.created = datetime.utcnow()
     package.file = file_name
+    package.category = category
     package.status = status
 
     package.save()
