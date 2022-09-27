9a1ef08dafc4caf47139dfb683158c5c825e72f8
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-27 18:07:10 -0300

    Refatora outras tasks para lidar com o ajuste feito no import

--- upload/tasks.py
+++ upload/tasks.py
@@ -153,25 +153,22 @@ def task_update_article_status_by_validations(task_id_article_erratum, task_id_c
 
 @celery_app.task(name='Validate article correction')
 def task_validate_article_correction(new_package_file_path, last_valid_package_file_path):
-    new_pkg_xmltree = packtools_package.PackageArticle(new_package_file_path).xmltree_article
-    last_valid_pkg_xmltree = packtools_package.PackageArticle(last_valid_package_file_path).xmltree_article
+    new_pkg_xmltree = sps_package.PackageArticle(new_package_file_path).xmltree_article
+    last_valid_pkg_xmltree = sps_package.PackageArticle(last_valid_package_file_path).xmltree_article
 
-    return packtools_article.are_similar_articles(new_pkg_xmltree, last_valid_pkg_xmltree)
+    return sps_validation_article.are_similar_articles(new_pkg_xmltree, last_valid_pkg_xmltree)
 
 @celery_app.task(name='Validate article erratum')
 def task_validate_article_erratum(file_path):
-    return packtools_package.PackageWithErrata(file_path).is_valid()
+    return sps_package.PackageWithErrata(file_path).is_valid()
 
 
 @celery_app.task(name='Compare packages')
 def task_compare_packages(package1_file_path, package2_file_path):   
-    pkg1_file_path = file_utils.get_file_absolute_path(package1_file_path)
-    pkg1_xmltree = packtools_package.PackageWithErrata(pkg1_file_path).xmltree_article
+    pkg1_xmltree = sps_package.PackageWithErrata(package1_file_path).xmltree_article
+    pkg2_xmltree = sps_package.PackageArticle(package2_file_path).xmltree_article
 
-    pkg2_file_path = file_utils.get_file_absolute_path(package2_file_path)
-    pkg2_xmltree = packtools_package.PackageArticle(pkg2_file_path).xmltree_article
-
-    return packtools_article.are_similar_articles(pkg1_xmltree, pkg2_xmltree)
+    return sps_validation_article.are_similar_articles(pkg1_xmltree, pkg2_xmltree)
 
 
 @celery_app.task()
