b2cd7a3e0d2b899ad4540b48d81ffa3ff586735a
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-09-28 16:34:59 -0300

    Exclui os modelos JournalCollections, IssueInCollections e DocumentInCollections

--- collection/models.py
+++ collection/models.py
@@ -29,15 +29,19 @@ class Collection(CommonControlField):
 
 class SciELOJournal(CommonControlField):
     """
-    Class that represent the Journal in a Collection
+    Class that represents journals data in a SciELO Collection context
+    Its attributes are related to the journal in collection
+    For official data, use Journal model
     """
-    scielo_issn = models.CharField(_('SciELO ISSN'), max_length=9, null=False, blank=False)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    scielo_issn = models.CharField(_('SciELO ISSN'), max_length=9, null=False, blank=False)
     acron = models.CharField(_('Acronym'), max_length=25, null=True, blank=True)
     title = models.CharField(_('Title'), max_length=255, null=True, blank=True)
     publication_status = models.CharField(
         _('Publication Status'), max_length=10, null=True, blank=True,
         choices=JOURNAL_PUBLICATION_STATUS)
+    official_journal = models.ForeignKey(
+    	OfficialJournal, on_delete=models.SET_NULL, null=True)
 
     # TODO acrescentar
     # data de entrada
@@ -63,30 +67,11 @@ class SciELOJournal(CommonControlField):
         return u'%s %s' % (self.collection, self.scielo_issn)
 
 
-class JournalCollections(CommonControlField):
-    """
-    Class that represent the journal and its collections
-    """
-
-    def __unicode__(self):
-        return u'%s %s' % (self.official_journal, " | ".join([str(item) for item in self.scielo_journals]))
-
-    def __str__(self):
-        return u'%s %s' % (self.official_journal, " | ".join([str(item) for item in self.scielo_journals]))
-
-    official_journal = models.ForeignKey(OfficialJournal, on_delete=models.CASCADE)
-    scielo_journals = models.ManyToManyField(SciELOJournal)
-
-    class Meta:
-
-        indexes = [
-            models.Index(fields=['official_journal']),
-        ]
-
-
 class SciELOIssue(CommonControlField):
     """
-    Class that represent an issue in a SciELO Collection
+    Class that represents an issue in a SciELO Collection
+    Its attributes are related to the issue in collection
+    For official data, use Issue model
     """
 
     def __unicode__(self):
@@ -95,11 +80,14 @@ class SciELOIssue(CommonControlField):
     def __str__(self):
         return u'%s %s' % (self.scielo_journal, self.issue_pid)
 
-    scielo_journal = models.ForeignKey(SciELOJournal, on_delete=models.CASCADE)
+    scielo_journal = models.ForeignKey(
+    	SciELOJournal, on_delete=models.SET_NULL, null=True)
     issue_pid = models.CharField(_('Issue PID'), max_length=17, null=False, blank=False)
     # v30n1 ou 2019nahead
     issue_folder = models.CharField(_('Issue Folder'), max_length=17, null=False, blank=False)
     pub_year = models.CharField(_('Publicatin year'), max_length=4, null=True, blank=True)
+    official_issue = models.ForeignKey(
+    	Issue, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         unique_together = [
@@ -115,29 +103,11 @@ class SciELOIssue(CommonControlField):
         ]
 
 
-class IssueInCollections(CommonControlField):
-    """
-    Class that represent an issue and its collections
-    """
-
-    def __unicode__(self):
-        return u'%s %s' % (self.official_issue, " | ".join([str(item) for item in self.scielo_issues]))
-
-    def __str__(self):
-        return u'%s %s' % (self.official_issue, " | ".join([str(item) for item in self.scielo_issues]))
-
-    official_issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
-    scielo_issues = models.ManyToManyField(SciELOIssue)
-
-    class Meta:
-        indexes = [
-            models.Index(fields=['official_issue']),
-        ]
-
-
 class SciELODocument(CommonControlField):
     """
-    Class that represent a document in a SciELO Collection
+    Class that represents a document in a SciELO Collection
+    Its attributes are related to the document in collection
+    For official data, use Article model
     """
 
     def __unicode__(self):
@@ -149,6 +119,7 @@ class SciELODocument(CommonControlField):
     scielo_issue = models.ForeignKey(SciELOIssue, on_delete=models.CASCADE)
     pid = models.CharField(_('PID'), max_length=23, null=True, blank=True)
     file_id = models.CharField(_('File ID'), max_length=17, null=True, blank=True)
+    official_document = models.ForeignKey(Article, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         unique_together = [
@@ -160,26 +131,6 @@ class SciELODocument(CommonControlField):
             models.Index(fields=['scielo_issue']),
             models.Index(fields=['pid']),
             models.Index(fields=['file_id']),
-        ]
-
-
-class DocumentInCollections(CommonControlField):
-    """
-    Class that represent a document and its collections
-    """
-
-    def __unicode__(self):
-        return u'%s %s' % (self.official_document, " | ".join([str(item) for item in self.scielo_documents]))
-
-    def __str__(self):
-        return u'%s %s' % (self.official_document, " | ".join([str(item) for item in self.scielo_documents]))
-
-    official_document = models.ForeignKey(Article, on_delete=models.CASCADE, null=True, blank=True)
-    scielo_documents = models.ManyToManyField(SciELODocument)
-
-    class Meta:
-        indexes = [
-            models.Index(fields=['official_document']),
         ]
 
 
