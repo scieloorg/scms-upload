86c8786232433464947837919c1b1473359b998a
Rafael JPD <rafael.pezzuto@gmail.com>
2022-10-09 13:01:05 -0300

    Acrescenta issue a article em save_all de form de upload.package

--- upload/forms.py
+++ upload/forms.py
@@ -4,11 +4,14 @@ from wagtail.admin.forms import WagtailAdminModelForm
 
 class UploadPackageForm(WagtailAdminModelForm):
 
-    def save_all(self, user):
+    def save_all(self, user, article, issue):
         upload_package = super().save(commit=False)
         
         if self.instance.pk is None:
             upload_package.creator = user
+
+        upload_package.article = article
+        upload_package.issue = issue
         
         self.save()
 
