a4100abe9fb8b206d1fb42919b8983aadc307681
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-10-12 17:23:16 -0300

    Melhara a forma de obter / criar dados oficiais de periódico

--- journal/controller.py
+++ journal/controller.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.utils.translation import gettext_lazy as _
 
 from journal.models import OfficialJournal
@@ -6,10 +8,10 @@ from . import exceptions
 
 
 arg_names = (
+    'title',
     'ISSN_electronic',
     'ISSN_print',
     'ISSNL',
-    'creator_id',
 )
 
 
@@ -21,19 +23,33 @@ def _get_args(names, values):
     }
 
 
-def get_or_create_official_journal(issn_l, e_issn, print_issn, creator_id):
-    if not any([e_issn, print_issn, issn_l]):
+def get_or_create_official_journal(title, issn_l, e_issn, print_issn, creator_id):
+    if not any([title, e_issn, print_issn, issn_l]):
         raise exceptions.GetOrCreateOfficialJournalError(
-            "collections.get_or_create_official_journal requires e_issn or print_issn or issn_l"
+            "collections.get_or_create_official_journal requires title or e_issn or print_issn or issn_l"
         )
 
-    kwargs = _get_args(arg_names, (e_issn, print_issn, issn_l, creator_id))
+    kwargs = _get_args(arg_names, (title, e_issn, print_issn, issn_l))
     try:
-        official_journal, status = OfficialJournal.objects.get_or_create(**kwargs)
+        logging.info("Get or create Official Journal {} {} {} {}".format(
+            title, issn_l, e_issn, print_issn
+        ))
+        official_journal = OfficialJournal.objects.get(**kwargs)
+        logging.info("Got {}".format(official_journal))
+    except OfficialJournal.DoesNotExist:
+        logging.info("DoesNotExist")
+        official_journal = OfficialJournal()
+        official_journal.title = title
+        official_journal.ISSNL = issn_l
+        official_journal.ISSN_electronic = e_issn
+        official_journal.ISSN_print = print_issn
+        official_journal.creator_id = creator_id
+        official_journal.save()
+        logging.info("Created {}".format(official_journal))
     except Exception as e:
         raise exceptions.GetOrCreateOfficialJournalError(
-            _('Unable to get or create official journal {} {} {} {} {}').format(
-                issn_l, e_issn, print_issn, type(e), e
+            _('Unable to get or create official journal {} {} {} {} {} {}').format(
+                title, issn_l, e_issn, print_issn, type(e), e
             )
         )
     return official_journal
@@ -44,7 +60,7 @@ def get_journal_dict_for_validation(journal_id):
 
     try:
         journal = OfficialJournal.objects.get(pk=journal_id)
-        data['titles'] = [journal.title,]
+        data['titles'] = [journal.title, ]
         data['print_issn'] = journal.ISSN_print
         data['electronic_issn'] = journal.ISSN_electronic
     except OfficialJournal.DoesNotExist:
