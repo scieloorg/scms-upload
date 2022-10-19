945520212eb20533235fb0003053c68236f96c24
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-10-19 15:39:54 -0300

    Adiciona módulo de exceções

---
+++ pid_provider/exceptions.py
@@ -0,0 +1,49 @@
+class ForbiddenUpdatingAOPVersionOfArticlePublishedInAnIssueError(Exception):
+    ...
+
+
+class FoundAOPPublishedInAnIssueError(Exception):
+    ...
+
+
+class GetRegisteredXMLError(Exception):
+    ...
+
+
+class GetXMLFromURIError(Exception):
+    ...
+
+
+class NotAllowedIngressingAOPVersionOfArticlePublishedInAnIssueError(Exception):
+    ...
+
+
+class NotEnoughParametersToGetDocumentRecordError(Exception):
+    ...
+
+
+class PutXMLError(Exception):
+    ...
+
+
+class RegisteredXMLDoesNotExistError(Exception):
+    ...
+
+
+class RequestDocumentIDsError(Exception):
+    ...
+
+
+class RequestDocumentIDsForXMLUriError(Exception):
+    ...
+
+
+class RequestDocumentIDsForXMLZipFileError(Exception):
+    ...
+
+
+
+class SavingError(Exception):
+    ...
+
+
