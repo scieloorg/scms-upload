fc9f004f55cc4d9233400c751e3b3a049e90c5a2
Rafael JPD <rafael.pezzuto@gmail.com>
2022-10-20 18:15:23 -0300

    Limita exibição de float a duas casas decimais

--- upload/wagtail_hooks.py
+++ upload/wagtail_hooks.py
@@ -270,12 +270,12 @@ class QualityAnalysisPackageAdmin(ModelAdmin):
 
     def stat_incapable_to_fix(self, obj):
         if obj.stat_incapable_to_fix_n:
-            return f"{obj.stat_incapable_to_fix_n} ({obj.stat_incapable_to_fix_p}%)"
+            return f"{obj.stat_incapable_to_fix_n} ({obj.stat_incapable_to_fix_p:.2f}%)"
         return '-'
 
     def stat_disagree(self, obj):
         if obj.stat_disagree_n:
-            return f"{obj.stat_disagree_n} ({obj.stat_disagree_p}%)"
+            return f"{obj.stat_disagree_n} ({obj.stat_disagree_p:.2f}%)"
         return '-'
 
     def get_queryset(self, request):
