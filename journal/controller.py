6a1841b662c68bc3b0327332a1fd2127b55aaa47
Rafael JPD <rafael.pezzuto@gmail.com>
2022-11-04 17:56:06 -0300

    Adiciona outros campos de título em dicionário de periódico

--- journal/controller.py
+++ journal/controller.py
@@ -60,7 +60,7 @@ def get_journal_dict_for_validation(journal_id):
 
     try:
         journal = OfficialJournal.objects.get(pk=journal_id)
-        data['titles'] = [journal.title, ]
+        data['titles'] = [t for t in [journal.title, journal.title_iso, journal.short_title, journal.nlm_title] if t is not None and len(t) > 0]
         data['print_issn'] = journal.ISSN_print
         data['electronic_issn'] = journal.ISSN_electronic
     except OfficialJournal.DoesNotExist:
