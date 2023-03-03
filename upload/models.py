270e6bc0d89f38f58b47f6d8833b2b0eb9ead336
rg3915 <regis42santos@gmail.com>
2023-03-03 07:26:14 -0300

    Troca CharField para TextField em upload: ValidationResult e ErrorResolution. close #138

--- upload/models.py
+++ upload/models.py
@@ -105,9 +105,7 @@ class ValidationResult(models.Model):
         blank=False,
     )
     data = models.JSONField(_("Error data"), default=dict, null=True, blank=True)
-    message = models.CharField(
-        _("Error message"), max_length=512, null=True, blank=True
-    )
+    message = models.TextField(_("Error message"), null=True, blank=True)
     status = models.CharField(
         _("Status"),
         max_length=16,
@@ -177,7 +175,7 @@ class ErrorResolution(CommonControlField):
         null=True,
         blank=True,
     )
-    rationale = models.TextField(_("Rationale"), max_length=512, null=True, blank=True)
+    rationale = models.TextField(_("Rationale"), null=True, blank=True)
 
     panels = [
         FieldPanel("action"),
