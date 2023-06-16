544a91db5f4fbafaaf4ecb3e48b8b498e28c5d2d
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-06-15 23:13:12 -0300

    Adiciona as dependência de pid_requester.models

---
+++ pid_requester/exceptions.py
@@ -0,0 +1,38 @@
+class APIPidProviderPostError(Exception):
+    ...
+
+
+class PidRequesterXMLContentError(Exception):
+    ...
+
+
+class ForbiddenPidRequesterXMLRegistrationError(Exception):
+    ...
+
+
+class GetAPITokenError(Exception):
+    ...
+
+
+class NotEnoughParametersToGetDocumentRecordError(Exception):
+    ...
+
+
+class QueryDocumentMultipleObjectsReturnedError(Exception):
+    ...
+
+
+class PidRequesterXMLCreateError(Exception):
+    ...
+
+
+class PidRequesterXMLUpdateError(Exception):
+    ...
+
+
+class PidRequesterXMLWithPreError(Exception):
+    ...
+
+
+class APIPidProviderConfigError(Exception):
+    ...
