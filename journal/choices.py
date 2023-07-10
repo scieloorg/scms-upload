1f2f9b3950efcf35ad6a0e08cca2ac3f1de278da
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-07-09 21:49:56 -0300

    Adiciona na app journal, em choices, JOURNAL_PUBLICATION_STATUS e JOURNAL_AVAILABILTY_STATUS

--- journal/choices.py
+++ journal/choices.py
@@ -5,3 +5,24 @@ SOCIAL_NETWORK_NAMES = [
     ("twitter", "Twitter"),
     ("journal", _("Journal URL")),
 ]
+
+
+CURRENT = "C"
+NOT_INFORMED = ""
+CEASED = "D"
+UNKNOWN = "?"
+SUSPENDED = "S"
+
+JOURNAL_PUBLICATION_STATUS = [
+    (SUSPENDED, _("Suspended")),
+    (UNKNOWN, _("Unknown")),
+    (CEASED, _("Ceased")),
+    (NOT_INFORMED, _("Not informed")),
+    (CURRENT, _("Current")),
+]
+
+# AVAILABILTY on the website
+JOURNAL_AVAILABILTY_STATUS = [
+    (UNKNOWN, _("Unknown")),
+    (CURRENT, _("Current")),
+]
