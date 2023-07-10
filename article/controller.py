2e94a5998fe458977a25387460b9ca76cda5dea7
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-07-09 22:20:50 -0300

    Renomeia e atualiza a função request_pid_v3_and_create_article que solicita PID versão 3 (pid v3)

--- article/controller.py
+++ article/controller.py
@@ -1,47 +1,49 @@
+import logging
 from datetime import datetime
 
 from django.utils.translation import gettext_lazy as _
 from packtools.sps.models.article_ids import ArticleIds
 
 from pid_requester.controller import PidRequester
-from xmlsps.xml_sps_lib import XMLWithPre
-
 from . import exceptions
 from .models import Article, choices
+from collection.models import Collection
 
 
-def request_pid(xml_with_pre, filename, user):
+def request_pid_v3_and_create_article(xml_with_pre, filename, user, collection=None):
     """
     Solicita PID versão 3, registra o artigo e o arquivo XML com o pid v3
     provido pelo PID Provider
+
+    collection: identificar coleção se requisição vier da migração
     """
+    logging.info(f"Request PID V3 para {filename}")
     pid_requester = PidRequester()
     response = pid_requester.request_pid_for_xml_with_pre(xml_with_pre, filename, user)
-    if response.get("error_type"):
-        return response
+
+    # IGNORA ERRO DE SOLICITACAO PID PROVIDER DO CORE
+    # TODO REMOVER O COMENTÁRIO FUTURAMENTE
+    # if response.get("error_type"):
+    #     return response
+
+    try:
+        issnl = xml_with_pre.journal_issnl
+    except AttributeError:
+        issnl = None
     article = Article.get_or_create(
         pid_v3=response["v3"],
         pid_v2=response["v2"],
         aop_pid=response.get("aop_pid"),
         creator=user,
-    )
-    article.add_pages(
-        xml_with_pre.fpage,
-        xml_with_pre.fpage_seq,
-        xml_with_pre.lpage,
-        xml_with_pre.elocation_id,
-    )
-    article.add_type(xml_with_pre.xmltree.find(".").get("article-type"))
-    article.add_issue(
+        issn_electronic=xml_with_pre.journal_issn_electronic,
+        issn_print=xml_with_pre.journal_issn_print,
+        issnl=issnl,
         volume=xml_with_pre.volume,
         number=xml_with_pre.number,
         suppl=xml_with_pre.suppl,
+        publication_year=xml_with_pre.pub_year,
+        collection=collection,
     )
-
-    article.add_xml_sps(filename, xml_with_pre.tostring())
-
-    for related in xml_with_pre.related_items:
-        article.add_related_item(related["href"], related["related-article-type"])
     return {"article": article}
 
 
@@ -77,15 +79,3 @@ def update_article(article_id, **kwargs):
             _("Unable to update article {} {} {}").format(str(kwargs), type(e), e)
         )
     return article
-
-
-def get_or_create_official_article(official_issue, **kwargs):
-    try:
-        official_article, status = Article.objects.get_or_create(**kwargs)
-    except Exception as e:
-        raise exceptions.GetOrCreateDocumentError(
-            _("Unable to get or create official article {} {} {}").format(
-                str(kwargs), type(e), e
-            )
-        )
-    return official_article
