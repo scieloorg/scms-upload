6f714a017ecc92655a813db3ebdb932477a1da2d
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-07-16 19:53:25 -0300

    Cria duas tasks: a) executar a migração dos registros e dos arquivos de documentos de vários fascículos; b) executar a migração dos registros e dos arquivos de 1 fascículo;

--- migration/tasks.py
+++ migration/tasks.py
@@ -1,9 +1,12 @@
+from django.db.models import Q
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
 from config import celery_app
 
 from . import controller
+from migration.models import MigratedIssue
+
 
 User = get_user_model()
 
@@ -67,6 +70,60 @@ def task_migrate_issue_files_and_document_records(
     user = _get_user(self.request, username)
     controller.migrate_issue_files_and_document_records(
         user,
+        collection_acron,
+        scielo_issn,
+        publication_year,
+        force_update,
+    )
+
+
+@celery_app.task(bind=True, name="migrate_set_of_issue_files_and_document_records")
+def task_migrate_set_of_issue_files_and_document_records(
+    self,
+    username,
+    collection_acron,
+    scielo_issn=None,
+    publication_year=None,
+    force_update=False,
+):
+    user = _get_user(self.request, username)
+
+    params = {"migrated_journal__scielo_journal__collection__acron": collection_acron}
+    if scielo_issn:
+        params["migrated_journal__scielo_journal__scielo_issn"] = scielo_issn
+    if publication_year:
+        params["scielo_issue__official_issue__publication_year"] = publication_year
+
+    items = MigratedIssue.objects.filter(
+        Q(status=MS_PUBLISHED) | Q(status=MS_IMPORTED), **params,
+    )
+    for migrated_issue in items.iterator():
+        task_migrate_one_issue_files_and_document_records.apply_async(
+            kwargs={
+                "username": username,
+                "migrated_issue_id": migrated_issue.id,
+                "collection_acron": collection_acron,
+                "scielo_issn": scielo_issn,
+                "publication_year": publication_year,
+                "force_update": force_update,
+            }
+        )
+
+
+@celery_app.task(bind=True, name="migrate_one_issue_files_and_document_records")
+def task_migrate_one_issue_files_and_document_records(
+    self,
+    username,
+    migrated_issue_id,
+    collection_acron,
+    scielo_issn=None,
+    publication_year=None,
+    force_update=False,
+):
+    user = _get_user(self.request, username)
+    controller.migrate_one_issue_files_and_document_records(
+        user,
+        migrated_issue_id,
         collection_acron,
         scielo_issn,
         publication_year,
