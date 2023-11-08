627524f28cd69a347bbed4b139934ebd993e8028
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-11-08 15:13:43 -0300

    Compatibiliza pid_provider.wagtail_hooks com Core

--- pid_provider/wagtail_hooks.py
+++ pid_provider/wagtail_hooks.py
@@ -7,7 +7,7 @@ from wagtail.contrib.modeladmin.options import (
 )
 from wagtail.contrib.modeladmin.views import CreateView
 
-from .models import PidChange, PidProviderConfig, PidProviderXML, PidRequest
+from .models import CollectionPidRequest, PidChange, PidProviderXML, PidRequest
 
 
 class PidRequestCreateView(CreateView):
@@ -31,12 +31,39 @@ class PidRequestAdmin(ModelAdmin):
         "origin",
         "result_type",
         "result_msg",
-        "created",
+        "v3",
+        "times",
+        "created_updated",
     )
     list_filter = ("result_type",)
     search_fields = (
         "origin",
+        "v3",
         "result_msg",
+    )
+
+
+class CollectionPidRequestAdmin(ModelAdmin):
+    list_per_page = 10
+    model = CollectionPidRequest
+    inspect_view_enabled = True
+    menu_label = _("Collection Pid Requests")
+    create_view_class = PidRequestCreateView
+    menu_icon = "folder"
+    menu_order = 300
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+
+    list_display = (
+        "collection",
+        "end_date",
+        "created",
+        "updated",
+    )
+    list_filter = []
+    search_fields = (
+        "collection__acron3",
+        "collection__name",
     )
 
 
@@ -63,8 +90,10 @@ class PidProviderXMLAdmin(ModelAdmin):
         "v2",
         "aop_pid",
         "main_doi",
+        "website_publication_date",
+        "created_updated",
     )
-    list_filter = ("article_pub_year",)
+    list_filter = ("article_pub_year", "pub_year")
     search_fields = (
         "pkg_name",
         "v3",
@@ -72,6 +101,7 @@ class PidProviderXMLAdmin(ModelAdmin):
         "aop_pid",
         "main_doi",
         "article_pub_year",
+        "website_publication_date",
     )
 
 
@@ -93,14 +123,16 @@ class PidChangeAdmin(ModelAdmin):
     exclude_from_explorer = False
 
     list_display = (
-        "old",
-        "new",
+        "pkg_name",
+        "pid_in_xml",
+        "pid_assigned",
         "pid_type",
+        "created_updated",
     )
     list_filter = ("pid_type",)
     search_fields = (
-        "old",
-        "new",
+        "pid_in_xml",
+        "pid_assigned",
     )
 
 
@@ -128,9 +160,9 @@ class PidProviderConfigAdmin(ModelAdmin):
 
 
 class PidProviderAdminGroup(ModelAdminGroup):
-    menu_label = _("Pid Requester")
+    menu_label = _("Pid Provider")
     menu_icon = "folder-open-inverse"  # change as required
-    menu_order = 100  # will put in 3rd place (000 being 1st, 100 2nd)
+    menu_order = 6
     items = (
         PidProviderConfigAdmin,
         PidProviderXMLAdmin,
