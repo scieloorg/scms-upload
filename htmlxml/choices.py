eecb21d956c72ac58249552a3a43fbb0d1c987bf
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-12-11 17:21:41 -0300

    Cria htmlxml.exceptions e htmlxml.choices

---
+++ htmlxml/choices.py
@@ -0,0 +1,13 @@
+from django.utils.translation import gettext as _
+
+HTML2XML_QA_AUTO_APPROVED = "AUTO_APPROVED"
+HTML2XML_QA_APPROVED = "APPROVED"
+HTML2XML_QA_REJECTED = "REJECTED"
+HTML2XML_QA_NOT_EVALUATED = "NOT_EVALUATED"
+
+HTML2XML_QA = (
+    (HTML2XML_QA_AUTO_APPROVED, _("approved automatically")),
+    (HTML2XML_QA_APPROVED, _("approved")),
+    (HTML2XML_QA_REJECTED, _("rejected")),
+    (HTML2XML_QA_NOT_EVALUATED, _("not evaluated")),
+)
---
+++ htmlxml/exceptions.py
@@ -0,0 +1,46 @@
+class HTMLXMLCreateOrUpdateError(Exception):
+    ...
+
+
+class CreateOrUpdateBodyAndBackFileError(Exception):
+    ...
+
+
+class ClassicWebsiteJournalDataGetError(Exception):
+    ...
+
+
+class CreateOrUpdateMigratedError(Exception):
+    ...
+
+
+class ScheduleMigrationsError(Exception):
+    ...
+
+
+class GetOrCreateCrontabScheduleError(Exception):
+    ...
+
+
+class CreateOrUpdateMigratedFileError(Exception):
+    ...
+
+
+class CreateOrUpdateMigratedJournalError(Exception):
+    ...
+
+
+class CreateOrUpdateMigratedIssueError(Exception):
+    ...
+
+
+class CreateOrUpdateMigratedDocumentError(Exception):
+    ...
+
+
+class CreateOrUpdateGeneratedXMLFileError(Exception):
+    ...
+
+
+class MigratedXMLFileNotFoundError(Exception):
+    ...
