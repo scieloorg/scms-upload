2b615e1ff4cc55bbcce4bbb51686fa83bca361d5
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-12-12 20:13:56 -0300

    Cria a tarefa que inicializa o sistema

---
+++ bigbang/tasks.py
@@ -0,0 +1,28 @@
+import logging
+
+from django.contrib.auth import get_user_model
+
+from bigbang import tasks_scheduler
+from bigbang.setup import setup
+from config import celery_app
+
+User = get_user_model()
+
+
+def _get_user(user_id, username):
+    if user_id:
+        return User.objects.get(pk=user_id)
+    if username:
+        return User.objects.get(username=username)
+
+
+@celery_app.task(bind=True)
+def task_start(
+    self, user_id=None, username=None, file_path=None, activate_subtasks=None
+):
+    user = _get_user(user_id, username)
+    setup(user, file_path)
+
+    # activate_subtasks = activate_subtasks or True
+    tasks_scheduler.schedule_publication_subtasks(username, activate_subtasks)
+    tasks_scheduler.schedule_migration_subtasks(username, activate_subtasks)
