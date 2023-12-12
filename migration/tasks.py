639d85762beaf1b48703e8a57640158c8fada28f
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-12-12 14:53:16 -0300

    Refatora migration.tasks

--- migration/tasks.py
+++ migration/tasks.py
@@ -1,195 +1,461 @@
+import logging
+import sys
+
 from django.contrib.auth import get_user_model
-from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 
+from collection.models import Collection
 from config import celery_app
-from migration.models import MigratedIssue
+from htmlxml.models import HTMLXML
+from proc.models import ArticleProc, IssueProc, JournalProc
+from tracker.models import UnexpectedEvent
 
 from . import controller
-from .choices import MS_IMPORTED, MS_PUBLISHED, MS_TO_IGNORE
 
 User = get_user_model()
 
 
-def _get_user(request, username):
+def _get_user(user_id, username):
     try:
-        return User.objects.get(pk=request.user.id)
-    except AttributeError:
-        return User.objects.get(username=username)
-
-
-@celery_app.task(bind=True, name=_("schedule_migrations"))
-def task_schedule_migrations(
-    self,
-    username=None,
-    collection_acron=None,
-):
-    user = _get_user(self.request, username)
-    controller.schedule_migrations(user, collection_acron)
-
-
-@celery_app.task(bind=True, name="migrate_journal_records")
-def task_migrate_journal_records(
-    self,
-    username,
-    collection_acron,
-    force_update=False,
-):
-    user = _get_user(self.request, username)
-    controller.migrate_journal_records(
-        user,
-        collection_acron,
-        force_update,
-    )
-
-
-@celery_app.task(bind=True, name="migrate_issue_records_and_files")
-def task_migrate_issue_records_and_files(
-    self,
-    username,
-    collection_acron,
-    force_update=False,
-):
-    user = _get_user(self.request, username)
-    controller.migrate_issue_records_and_files(
-        user,
-        collection_acron,
-        force_update,
-    )
-
-
-@celery_app.task(bind=True, name="migrate_set_of_issue_files")
-def task_migrate_set_of_issue_files(
-    self,
-    username,
-    collection_acron,
-    scielo_issn=None,
-    publication_year=None,
-    force_update=False,
-):
-    user = _get_user(self.request, username)
-
-    params = {"migrated_journal__scielo_journal__collection__acron": collection_acron}
-    if scielo_issn:
-        params["migrated_journal__scielo_journal__scielo_issn"] = scielo_issn
-    if publication_year:
-        params["scielo_issue__official_issue__publication_year"] = publication_year
-
-    items = MigratedIssue.objects.filter(
-        Q(status=MS_PUBLISHED) | Q(status=MS_IMPORTED),
-        **params,
-    )
-    for migrated_issue in items.iterator():
-        task_migrate_one_issue_files.apply_async(
-            kwargs={
+        if user_id:
+            return User.objects.get(pk=user_id)
+        if username:
+            return User.objects.get(username=username)
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks._get_user",
+                "user_id": user_id,
                 "username": username,
-                "migrated_issue_id": migrated_issue.id,
-                "collection_acron": collection_acron,
-                "force_update": force_update,
-            }
+            },
         )
 
 
-@celery_app.task(bind=True, name="migrate_one_issue_files")
-def task_migrate_one_issue_files(
-    self,
-    username,
-    migrated_issue_id,
-    collection_acron,
-    force_update=False,
-):
-    user = _get_user(self.request, username)
-    migrated_issue = MigratedIssue.objects.get(id=migrated_issue_id)
-    controller.migrate_one_issue_files(
-        user,
-        migrated_issue,
-        collection_acron,
-        force_update,
-    )
-
-
-@celery_app.task(bind=True, name="migrate_set_of_issue_document_records")
-def task_migrate_set_of_issue_document_records(
-    self,
-    username,
-    collection_acron,
-    scielo_issn=None,
-    publication_year=None,
-    force_update=False,
-):
-    user = _get_user(self.request, username)
-
-    params = {"migrated_journal__scielo_journal__collection__acron": collection_acron}
-    if scielo_issn:
-        params["migrated_journal__scielo_journal__scielo_issn"] = scielo_issn
-    if publication_year:
-        params["scielo_issue__official_issue__publication_year"] = publication_year
-
-    items = MigratedIssue.objects.filter(
-        Q(status=MS_PUBLISHED) | Q(status=MS_IMPORTED),
-        **params,
-    )
-    for migrated_issue in items.iterator():
-        task_migrate_one_issue_document_records.apply_async(
-            kwargs={
-                "username": username,
-                "migrated_issue_id": migrated_issue.id,
+def _get_collections(collection_acron):
+    try:
+        if collection_acron:
+            return Collection.objects.filter(acron=collection_acron).iterator()
+        else:
+            return Collection.objects.iterator()
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks._get_collections",
                 "collection_acron": collection_acron,
-                "force_update": force_update,
-            }
+            },
         )
 
 
-@celery_app.task(bind=True, name="migrate_one_issue_document_records")
-def task_migrate_one_issue_document_records(
+@celery_app.task(bind=True)
+def task_migrate_title_databases(
     self,
-    username,
-    migrated_issue_id,
-    collection_acron,
-    force_update=False,
-):
-    user = _get_user(self.request, username)
-    migrated_issue = MigratedIssue.objects.get(id=migrated_issue_id)
-    controller.migrate_one_issue_document_records(
-        user,
-        migrated_issue,
-        collection_acron,
-        force_update,
-    )
-
-
-@celery_app.task(bind=True, name=_("run_migrations"))
-def task_run_migrations(
-    self,
+    user_id=None,
     username=None,
     collection_acron=None,
     force_update=False,
 ):
-    user = _get_user(self.request, username)
-    # migra os registros da base TITLE
-    task_migrate_journal_records.apply_async(
-        kwargs={
-            "username": username,
-            "collection_acron": collection_acron,
-            "force_update": force_update,
-        }
-    )
-    # migra os registros da base ISSUE
-    # migra os arquivos contidos nas pastas dos fascículos
-    task_migrate_issue_records_and_files.apply_async(
-        kwargs={
-            "username": username,
-            "collection_acron": collection_acron,
-            "force_update": force_update,
-        }
-    )
-    # migra os registros das bases de artigos
-    # se aplicável, gera XML a partir do HTML
-    # gera SPS package
-    task_migrate_set_of_issue_document_records.apply_async(
-        kwargs={
-            "username": username,
-            "collection_acron": collection_acron,
-            "force_update": force_update,
-        }
-    )
+    """
+    Para todas ou para uma dada coleção,
+    aciona uma tarefa para migrar a base de dados "title"
+
+    Parameters
+    ----------
+    user_id : int
+        identificacao do usuário
+    username : str
+        identificacao do usuário
+    collection_acron : str
+        acrônimo da coleção
+    force_update : bool
+        atualiza mesmo se já existe
+    """
+    try:
+        for collection in _get_collections(collection_acron):
+
+            # obtém os dados do site clássico
+            classic_website = controller.get_classic_website(collection.acron)
+
+            for (
+                scielo_issn,
+                journal_data,
+            ) in classic_website.get_journals_pids_and_records():
+                # para cada registro da base de dados "title",
+                # cria um registro MigratedData (source="journal")
+                task_migrate_title_record.apply_async(
+                    kwargs=dict(
+                        user_id=user_id,
+                        username=username,
+                        collection_acron=collection.acron,
+                        pid=scielo_issn,
+                        data=journal_data[0],
+                        force_update=force_update,
+                    )
+                )
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks._get_collections",
+                "user_id": user_id,
+                "username": username,
+                "collection_acron": collection_acron,
+                "force_update": force_update,
+            },
+        )
+
+
+@celery_app.task(bind=True)
+def task_migrate_title_record(
+    self,
+    user_id=None,
+    username=None,
+    collection_acron=None,
+    pid=None,
+    data=None,
+    force_update=False,
+):
+    """
+    Cria um registro MigratedData (source="journal")
+    """
+    try:
+        user = _get_user(user_id, username)
+        collection = Collection.get(acron=collection_acron)
+        JournalProc.register_classic_website_data(
+            user,
+            collection,
+            pid,
+            data,
+            "journal",
+            force_update,
+        )
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks._get_collections",
+                "user_id": user_id,
+                "username": username,
+                "collection_acron": collection_acron,
+                "pid": pid,
+                "data": data,
+                "force_update": force_update,
+            },
+        )
+
+
+@celery_app.task(bind=True)
+def task_migrate_issue_databases(
+    self,
+    user_id=None,
+    username=None,
+    collection_acron=None,
+    force_update=False,
+):
+    """
+    Para todas ou para uma dada coleção,
+    aciona uma tarefa para migrar a base de dados "issue"
+
+    Parameters
+    ----------
+    user_id : int
+        identificacao do usuário
+    username : str
+        identificacao do usuário
+    collection_acron : str
+        acrônimo da coleção
+    force_update : bool
+        atualiza mesmo se já existe
+    """
+
+    try:
+        for collection in _get_collections(collection_acron):
+            # obtém os dados do site clássico
+            classic_website = controller.get_classic_website(collection.acron)
+
+            for (
+                pid,
+                issue_data,
+            ) in classic_website.get_issues_pids_and_records():
+                # para cada registro da base de dados "issue",
+                # cria um registro MigratedData (source="issue")
+                task_migrate_issue_record.apply_async(
+                    kwargs=dict(
+                        user_id=user_id,
+                        username=username,
+                        collection_acron=collection.acron,
+                        pid=pid,
+                        data=issue_data[0],
+                        force_update=force_update,
+                    )
+                )
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks.task_migrate_issue_db",
+                "user_id": user_id,
+                "username": username,
+                "collection_acron": collection_acron,
+                "force_update": force_update,
+            },
+        )
+
+
+@celery_app.task(bind=True)
+def task_migrate_issue_record(
+    self,
+    user_id=None,
+    username=None,
+    collection_acron=None,
+    pid=None,
+    data=None,
+    force_update=False,
+):
+    """
+    Cria um registro MigratedData (source="issue")
+    """
+    try:
+        user = _get_user(user_id, username)
+        collection = Collection.get(acron=collection_acron)
+
+        IssueProc.register_classic_website_data(
+            user,
+            collection,
+            pid,
+            data,
+            "issue",
+            force_update,
+        )
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks.task_migrate_issue_record",
+                "user_id": user_id,
+                "username": username,
+                "collection_acron": collection_acron,
+                "pid": pid,
+                "data": data,
+                "force_update": force_update,
+            },
+        )
+
+
+############################################
+@celery_app.task(bind=True)
+def task_migrate_document_files(
+    self,
+    user_id=None,
+    username=None,
+    collection_acron=None,
+    force_update=False,
+):
+    try:
+        for collection in _get_collections(collection_acron):
+            items = IssueProc.files_to_migrate(collection, force_update)
+            for item in items:
+                # Importa os arquivos das pastas */acron/volnum/*
+                task_import_one_issue_files.apply_async(
+                    kwargs=dict(
+                        user_id=user_id,
+                        username=username,
+                        item_id=item.id,
+                        force_update=force_update,
+                    )
+                )
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks.task_migrate_document_files",
+                "user_id": user_id,
+                "username": username,
+                "collection_acron": collection_acron,
+                "force_update": force_update,
+            },
+        )
+
+
+@celery_app.task(bind=True)
+def task_import_one_issue_files(
+    self,
+    user_id=None,
+    username=None,
+    item_id=None,
+    force_update=False,
+):
+    try:
+        user = _get_user(user_id, username)
+        item = IssueProc.objects.get(pk=item_id)
+        item.get_files_from_classic_website(
+            user, force_update, controller.import_one_issue_files
+        )
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks.task_import_one_issue_files",
+                "user_id": user_id,
+                "username": username,
+                "item_id": item_id,
+                "force_update": force_update,
+            },
+        )
+
+
+############################################
+@celery_app.task(bind=True)
+def task_migrate_document_records(
+    self,
+    user_id=None,
+    username=None,
+    collection_acron=None,
+    force_update=False,
+):
+    try:
+        for collection in _get_collections(collection_acron):
+            items = IssueProc.docs_to_migrate(collection, force_update)
+            for item in items:
+                # Importa os registros de documentos
+                task_import_one_issue_document_records.apply_async(
+                    kwargs=dict(
+                        item_id=item.id,
+                        user_id=user_id,
+                        username=username,
+                        force_update=force_update,
+                    )
+                )
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks.task_migrate_document_records",
+                "user_id": user_id,
+                "username": username,
+                "collection_acron": collection_acron,
+                "force_update": force_update,
+            },
+        )
+
+
+@celery_app.task(bind=True)
+def task_import_one_issue_document_records(
+    self,
+    item_id,
+    user_id=None,
+    username=None,
+    force_update=False,
+):
+    """
+    Cria ou atualiza os registros de ArticleProc
+    """
+    try:
+        user = _get_user(user_id, username)
+        item = IssueProc.objects.get(pk=item_id)
+        item.get_article_records_from_classic_website(
+            user, force_update, controller.get_article_records_from_classic_website
+        )
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks.task_import_one_issue_document_records",
+                "item_id": item_id,
+                "user_id": user_id,
+                "username": username,
+                "force_update": force_update,
+            },
+        )
+
+
+@celery_app.task(bind=True)
+def task_get_xmls(
+    self,
+    user_id=None,
+    username=None,
+    collection_acron=None,
+    force_update=False,
+):
+    try:
+        for collection in _get_collections(collection_acron):
+            items = ArticleProc.items_to_get_xml(
+                collection_acron=collection_acron,
+                force_update=force_update,
+            )
+            for item in items:
+                task_get_xml.apply_async(
+                    kwargs={
+                        "username": username,
+                        "user_id": user_id,
+                        "item_id": item.id,
+                        "body_and_back_xml": force_update,
+                    }
+                )
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks.task_get_xmls",
+                "user_id": user_id,
+                "username": username,
+                "collection_acron": collection_acron,
+                "force_update": force_update,
+            },
+        )
+
+
+@celery_app.task(bind=True)
+def task_get_xml(
+    self,
+    user_id=None,
+    username=None,
+    item_id=None,
+    body_and_back_xml=None,
+):
+    try:
+        user = _get_user(user_id, username)
+
+        item = ArticleProc.objects.get(pk=item_id)
+        try:
+            htmlxml = HTMLXML.get(article_proc=item)
+        except HTMLXML.DoesNotExist:
+            htmlxml = None
+        item.get_xml(user, htmlxml, body_and_back_xml)
+
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            e=e,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "migration.tasks.task_get_xml",
+                "item_id": item_id,
+                "user_id": user_id,
+                "username": username,
+                "body_and_back_xml": body_and_back_xml,
+            },
+        )
