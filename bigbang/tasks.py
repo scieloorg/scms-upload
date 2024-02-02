65542dd2f7d7db1fbf2406a7aa55f5be2762607c
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-02-02 16:59:41 -0300

    Separa a task_start em duas, uma dedicada ao agendamento e outra à configuração

--- bigbang/tasks.py
+++ bigbang/tasks.py
@@ -18,14 +18,22 @@ def _get_user(user_id, username):
 
 @celery_app.task(bind=True)
 def task_start(
-    self, user_id=None, username=None, file_path=None, enable=False,
-    config=None
+    self, user_id=None, username=None, enable=False,
 ):
-    user = _get_user(user_id, username)
-
-    if file_path or config:
-        setup(user, file_path, config)
-
     tasks_scheduler.schedule_publication_subtasks(username, enabled=enable)
     tasks_scheduler.schedule_migration_subtasks(username, enabled=enable)
     # FIXME tasks_scheduler.schedule_task_synchronize_to_pid_provider(username, enabled=enable)
+
+
+@celery_app.task(bind=True)
+def task_setup(
+    self, 
+    user_id=None,
+    username=None,
+    file_path=None,
+    config=None,
+):
+    user = _get_user(user_id, username)    
+    
+    if file_path or config:
+        setup(user, file_path, config)
