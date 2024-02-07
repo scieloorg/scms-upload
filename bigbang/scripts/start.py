5ddfc461d512b40633934b5c8224f10bd89937f5
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-02-07 16:41:44 -0300

    remove parametro file_path

--- bigbang/scripts/start.py
+++ bigbang/scripts/start.py
@@ -1,12 +1,11 @@
 from bigbang.tasks import task_start
 
 
-def run(username, file_path, status):
+def run(username, status):
 
     task_start.apply_async(
         kwargs=dict(
             username=username,
-            file_path=file_path,
             enable=(status == "true"),
         )
     )
