8258842e2d470155d42e5938ace99063189db9e0
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-02-07 16:41:28 -0300

    REaloca _get_user em uma area comum

---
+++ core/utils/get_user.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+def _get_user(user_id, username):
+    if user_id:
+        return User.objects.get(pk=user_id)
+    if username:
+        return User.objects.get(username=username)
