8c80d3cd3b85173a447e7aa8f695ab6d1db4f64a
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-03-19 10:38:13 -0300

    Refatora a funcionalidade da ordem do menu

--- config/menu.py
+++ config/menu.py
@@ -1,14 +1,27 @@
-WAGTAIL_MENU_APPS_ORDER = {
-    "collection": 400,
-    "journal": 500,
-    "issue": 510,
-    "article": 520,
-    "upload": 700,
-    "migration": 710,
-    "location": 800,
-    "institution": 810,
-}
 
+WAGTAIL_MENU_APPS_ORDER = [
+    "collection",
+    "pid_provider",
+    "migration",
+    "processing",
+    "Tarefas",
+    "journal",
+    "issue",
+    "institution",
+    "article",
+    "researcher",
+    "location",
+    "unexpected-error",
+    "Configurações",
+    "Relatórios",
+    "Ajuda",
+    "Images",
+    "Documentos",
+]
 
 def get_menu_order(app_name):
-    return WAGTAIL_MENU_APPS_ORDER.get(app_name) or 100
+    try:
+        return WAGTAIL_MENU_APPS_ORDER.index(app_name) + 1
+    except:
+        return 9000
+
