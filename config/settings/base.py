a259b996e282c3f36362803a3f88dd7cfcb90b87
Samuel Veiga Rangel <82840278+samuelveigarangel@users.noreply.github.com>
2024-03-24 10:52:39 -0300

    Atualiza dependencias base.txt e production.txt (#409)
    * Comenta app captcha
    
    * Atualiza dependencias
    
    ---------
    
    Co-authored-by: Roberta Takenaka <505143+robertatakenaka@users.noreply.github.com>

--- config/settings/base.py
+++ config/settings/base.py
@@ -101,7 +101,7 @@ THIRD_PARTY_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "django_celery_beat",
-    "captcha",
+    # "captcha",
     "wagtailcaptcha",
     "wagtailmenus",
     "rest_framework",
