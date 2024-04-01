95c5012f0d47967061517a2c223b6178aa060140
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-04-01 15:50:48 -0300

    Cria as tasks “try_fetch_and_register_press_release” e “register_press_release”

---
+++ core/tasks.py
@@ -0,0 +1,79 @@
+import logging
+import feedparser
+from datetime import datetime
+from config import celery_app
+
+from core.utils.requester import fetch_data
+from core.models import PressRelease
+from collection.models import Language
+from pid_provider.tasks import _get_user
+from journal.models import Journal
+
+
+RSS_PRESS_RELEASES_FEEDS_BY_CATEGORY = {
+    'pt_BR': {
+        'display_name': 'SciELO em Perspectiva Press Releases',
+        'url': 'https://pressreleases.scielo.org/blog/category/{1}/feed/'
+    },
+    'es': {
+        'display_name': 'SciELO en Perspectiva Press Releases',
+        'url': 'https://pressreleases.scielo.org/{0}/category/press-releases/{1}/feed/',
+    },
+    'en': {
+        'display_name': 'SciELO in Perspective Press Releases',
+        'url': 'https://pressreleases.scielo.org/{0}/category/press-releases/{1}/feed/',
+    },
+}
+
+
+
+@celery_app.task(bind=True)
+def try_fetch_and_register_press_release(self, username=None, user_id=None):
+    for journal in Journal.objects.all():
+        for lang, url in RSS_PRESS_RELEASES_FEEDS_BY_CATEGORY.items():
+            if journal.journal_acron:
+                press_release_url_by_lang = url.get("url").format(lang, journal.journal_acron)
+
+                response = fetch_data(press_release_url_by_lang, json=False, timeout=2, verify=False)
+                content = feedparser.parse(response)
+
+                if content.bozo:
+                    logging.error(
+                        "Could not parse feed content from '%s'. During processing this error '%s' was thrown.",
+                        press_release_url_by_lang,
+                        content.bozo_exception,
+                    )
+                
+                for entry in content.get("entries", []):
+                    register_press_release.apply_async(kwargs=dict(
+                        journal_id=journal.id,
+                        lang_code=lang,
+                        entry=entry,
+                        username=username,
+                        user_id=user_id,
+                    ))
+
+
+@celery_app.task(bind=True)
+def register_press_release(self, journal_id, lang_code, entry, username, user_id):
+    user = _get_user(self.request, username, user_id)
+    journal = Journal.objects.get(id=journal_id)
+    lang = Language.get(code2=lang_code)
+    published = entry.get("published") 
+    publication_date = datetime.strptime(published, '%a, %d %b %Y %H:%M:%S %z')
+    
+    try:
+        media_content = entry.get("media_content")[0].get("url")
+    except AttributeError:
+        pass
+
+    PressRelease.create_or_update(
+        url=entry.get("id"),
+        journal=journal,
+        language=lang,
+        title=entry.get("title"),
+        content=entry.get("summary"),
+        media_content=media_content,
+        publication_date=publication_date,
+        user=user,
+    )
