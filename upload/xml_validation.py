1aad563d870414756eedf2d9c5bd099bbbadfcc5
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-04-15 16:09:25 -0300

    Troca a categoria por grupo com o mesmo propósito que é agrupar as validações, tornar relatórios menores, mais contextualizados

--- upload/xml_validation.py
+++ upload/xml_validation.py
@@ -94,30 +94,34 @@ def validate_xml_content(sps_pkg_name, xmltree, data):
     # VE_DATA_CONSISTENCY_ERROR: data consistency
     # VE_CRITERIA_ISSUES_ERROR: required by the criteria document
 
-    error_category_and_function_items = (
-        (choices.VE_BIBLIOMETRICS_DATA_ERROR, validate_affiliations),
-        (choices.VE_BIBLIOMETRICS_DATA_ERROR, validate_authors),
-        (choices.VE_BIBLIOMETRICS_DATA_ERROR, validate_languages),
-        (choices.VE_CRITERIA_ISSUES_ERROR, validate_article_attributes),
-        (choices.VE_CRITERIA_ISSUES_ERROR, validate_data_availability),
-        (choices.VE_CRITERIA_ISSUES_ERROR, validate_licenses),
-        (choices.VE_DATA_CONSISTENCY_ERROR, validate_article_id_other),
-        (choices.VE_DATA_CONSISTENCY_ERROR, validate_article_languages),
-        (choices.VE_DATA_CONSISTENCY_ERROR, validate_article_type),
-        (choices.VE_DATA_CONSISTENCY_ERROR, validate_dates),
-        (choices.VE_DATA_CONSISTENCY_ERROR, validate_doi),
-        (choices.VE_DATA_CONSISTENCY_ERROR, validate_journal),
-        (choices.VE_DATA_CONSISTENCY_ERROR, validate_preprint),
-        (choices.VE_DATA_CONSISTENCY_ERROR, validate_related_articles),
-        (choices.VE_DATA_CONSISTENCY_ERROR, validate_subjects),
-        (choices.VE_DATA_CONSISTENCY_ERROR, validate_toc_sections),
-        (choices.VE_DATA_CONSISTENCY_ERROR, validate_xref),
+    validation_group_and_function_items = (
+        ("affiliations", validate_affiliations),
+        ("authors", validate_authors),
+        ("article", validate_languages),
+        ("article", validate_article_attributes),
+        ("open science", validate_data_availability),
+        ("open science", validate_licenses),
+        ("article", validate_article_id_other),
+        ("article", validate_article_languages),
+        ("article", validate_article_type),
+        ("dates", validate_dates),
+        ("article", validate_doi),
+        ("journal", validate_journal),
+        ("open science", validate_preprint),
+        ("related", validate_related_articles),
+        ("article", validate_subjects),
+        ("article", validate_toc_sections),
+        ("text", validate_xref),
+        # outras conforme forem adicionadas ao packtools
     )
-    for error_category, f in error_category_and_function_items:
+
+    for validation_group, f in validation_group_and_function_items:
         for item in f(sps_pkg_name, xmltree, data):
+            error_category = None
             if item["validation_type"] in ("value in list", "value", "match"):
                 error_category = choices.VE_DATA_CONSISTENCY_ERROR
             item["error_category"] = item.get("error_category") or error_category
+            item["group"] = validation_group
             yield item
 
 
