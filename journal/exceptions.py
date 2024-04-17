6629494fe2a98fe79e1823e77b8b77854161b3e0
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-04-17 15:29:56 -0300

    Adicao das excecoes de missao e subject

--- journal/exceptions.py
+++ journal/exceptions.py
@@ -4,3 +4,18 @@ class GetOrCreateOfficialJournalError(Exception):
 
 class JournalProcUpdateError(Exception):
     ...
+
+
+class MissionGetError(Exception):
+    ...
+
+
+class MissionCreateOrUpdateError(Exception):
+    ...
+
+
+class SubjectCreationOrUpdateError(Exception):
+    def __init__(self, code, message):
+        super().__init__(
+            f"Unable to create or update Subject with code: {code}: {str(message)}"
+        )
