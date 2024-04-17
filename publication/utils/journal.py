8aa56b4435a2135bd8c09a9d92cfb30751709270
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-04-17 15:32:16 -0300

    Muda os dados de migrated_data para os valores relacionado ao modelo Journal

--- publication/utils/journal.py
+++ publication/utils/journal.py
@@ -1,10 +1,10 @@
+from datetime import datetime
 from scielo_classic_website.models.journal import Journal as ClassicJournal
 
 
 def build_journal(journal_proc, builder):
     journal_id = journal_proc.pid
     migrated_journal_data = journal_proc.migrated_data.data
-    classic_j = ClassicJournal(migrated_journal_data)
 
     journal = journal_proc.journal
     official_journal = journal.official_journal
@@ -12,36 +12,24 @@ def build_journal(journal_proc, builder):
     builder.add_ids(journal_id)
     builder.add_dates(journal_proc.created, journal_proc.updated)
     builder.add_acron(journal_proc.acron)
-    builder.add_contact(
-        name=", ".join(classic_j.publisher_name),
-        email=classic_j.publisher_email,
-        address=", ".join(classic_j.publisher_address),
-        city=classic_j.publisher_city,
-        state=classic_j.publisher_state,
-        # TODO country name?
-        country=classic_j.publisher_country,
-    )
+    for publisher in journal.publisher.all():
+        builder.add_contact(
+            name=publisher.institution.name,
+            address=publisher.institution.location,
+            city=publisher.institution.location.city.name,
+            state=publisher.institution.location.state.name,
+            country=publisher.institution.location.country.name,
+        )
 
-    # TODO
-    # builder.add_issue_count(issue_count)
-    # TODO
-    # builder.add_metrics(total_h5_index, total_h5_median, h5_metric_year)
-    for item in classic_j.mission:
-        builder.add_mission(item["language"], item["text"])
-
-    # TODO
-    for item in classic_j.status_history:
-        # FIXME ver os demais valores:
-        # "interrupted": __("indexação interrompida pelo Comitê"),
-        # "finished": __("publicação finalizada"),
-        if item["status"] == "C":
-            item["status"] = "current"
-        elif item["status"] == "D":
-            item["status"] = "deceased"
-        elif item["status"] == "S":
-            item["status"] = "suspended"
-
-        builder.add_event_to_timeline(item["status"], item["date"], item.get("reason"))
+    for mission in journal.mission.all():
+        builder.add_mission(mission.language, mission.text)
+        # builder.add_event_to_timeline(item["status"], item["date"], item.get("reason"))
+    for journal_history in journal.journal_history.all():
+        builder.add_event_to_timeline(
+            journal_history.event_type,
+            journal.date,
+            journal.reason,
+        )
     builder.add_journal_issns(
         scielo_issn=journal_id,
         eletronic_issn=official_journal.issn_electronic,
@@ -57,13 +45,16 @@ def build_journal(journal_proc, builder):
         builder.add_logo_url(journal.logo_url)
     except AttributeError:
         builder.add_logo_url("https://www.scielo.org/journal_logo_missing.gif")
-    builder.add_online_submission_url(classic_j.submission_url)
+    
+    builder.add_online_submission_url(journal.submission_online_url) #Adicionar
     # TODO
     # builder.add_related_journals(previous_journal, next_journal_title)
-    for item in classic_j.sponsors:
-        builder.add_sponsor(item)
-    builder.add_thematic_scopes(
-        subject_categories=None,
-        subject_areas=classic_j.subject_areas,
-    )
+    for sponsor in journal.sponsor.all(): #Adicionar
+        builder.add_sponsor(sponsor.institution.name)
+    
+    for subject_area in journal.subject.all():
+        builder.add_thematic_scopes(
+            subject_categories=None,
+            subject_areas=subject_area.value,
+        )
     # builder.add_is_public()
