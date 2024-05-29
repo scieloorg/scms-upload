79a4e0fd2671ab8278f1f0c2b1ac7a46d080f1d7
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-05-29 12:29:41 -0300

    Cria XMLErrorReportForm e cria UploadPackageForm.save

--- upload/forms.py
+++ upload/forms.py
@@ -1,13 +1,27 @@
+import logging
+
 from django import forms
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.translation import gettext_lazy as _
 from wagtail.admin.forms import WagtailAdminModelForm
+
+from upload import choices
 
 
 class UploadPackageForm(WagtailAdminModelForm):
+    def save(self, commit=True):
+        upload_package = super().save(commit=False)
+        if upload_package.qa_decision:
+            upload_package.status = self.instance.qa_decision
+        upload_package.save()
+        return upload_package
+
     def save_all(self, user):
         upload_package = super().save(commit=False)
 
         if self.instance.pk is None:
             upload_package.creator = user
+            upload_package.save()
 
         self.save()
 
@@ -26,8 +40,32 @@ class ValidationResultForm(WagtailAdminModelForm):
         return vr_obj
 
 
-ErrorNegativeReactionForm = ValidationResultForm
-ErrorNegativeReactionDecisionForm = ValidationResultForm
+class XMLErrorReportForm(WagtailAdminModelForm):
+    def clean(self):
+        cleaned_data = super().clean()
+        if not cleaned_data.get("xml_producer_ack"):
+            self.add_error(
+                "xml_producer_ack", _("Inform if you finish or not the errors review")
+            )
+
+    def save_all(self, user):
+        vr_obj = super().save(commit=False)
+
+        if self.instance.pk is None:
+            vr_obj.creator = user
+
+        self.save()
+
+        return vr_obj
+
+    def save(self, commit=True):
+        report = super().save(commit=False)
+        if report.package.creator == report.updated_by:
+            report.package.calculate_xml_error_declaration_numbers()
+            if report.xml_producer_ack:
+                report.conclusion = choices.REPORT_CONCLUSION_DONE
+            report.package.save()
+        return report
 
 
 class ValidationResultErrorResolutionForm(forms.Form):
