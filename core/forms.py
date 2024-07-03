3b507a3328faf154b284e5f58f78797c61e32d2d
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-07-03 19:24:32 -0300

    Em CoreModelAdmin, atualiza creator, created, updated_by, updated

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,18 @@
+from datetime import datetime
+
 from wagtail.admin.forms import WagtailAdminModelForm
 
 
 class CoreAdminModelForm(WagtailAdminModelForm):
     def save_all(self, user):
         model_with_creator = super().save(commit=False)
-        model_with_creator.creator = user
+
+        if self.instance.pk is None:
+            model_with_creator.creator = user
+            model_with_creator.created = datetime.utcnow()
+        else:
+            model_with_creator.updated_by = user
+            model_with_creator.updated = datetime.utcnow()
         self.save()
 
         return model_with_creator
