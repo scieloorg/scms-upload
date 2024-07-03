80efe1fdf40c560807fc41a35650e385508ed70f
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-07-03 20:58:52 -0300

    Ao salvar os dados do formulário, coordenar os valores dos status das etapas seguintes

--- proc/forms.py
+++ proc/forms.py
@@ -1,13 +1,9 @@
-from wagtail.admin.forms import WagtailAdminModelForm
+from core.forms import CoreAdminModelForm
 
 
-class ProcAdminModelForm(WagtailAdminModelForm):
+class ProcAdminModelForm(CoreAdminModelForm):
     def save_all(self, user):
         s = super().save(commit=False)
-
-        if self.instance.pk is None:
-            s.creator = user
-
-        self.save()
-
+        s.set_status()
+        s.save()
         return s
