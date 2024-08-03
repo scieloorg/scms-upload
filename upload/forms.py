666cead1f5a326f36c806579bda1ddf3aa386315
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-08-03 17:40:56 -0300

    Modifica os formulários relacionados com as validações e adiciona formulários de pacote em situação de análise manual e aprovados

--- upload/forms.py
+++ upload/forms.py
@@ -1,44 +1,96 @@
+import logging
+from datetime import datetime
+
 from django import forms
-from wagtail.admin.forms import WagtailAdminModelForm
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.translation import gettext_lazy as _
+
+from core.forms import CoreAdminModelForm
+from team.models import CollectionTeamMember
+from upload import choices
 
 
-class UploadPackageForm(WagtailAdminModelForm):
-    def save_all(self, user, article, issue):
-        upload_package = super().save(commit=False)
-
-        if self.instance.pk is None:
-            upload_package.creator = user
-
-        if article is not None:
-            upload_package.article = article
-
-        if issue is not None:
-            upload_package.issue = issue
-
-        self.save()
-
-        return upload_package
+class UploadPackageForm(CoreAdminModelForm):
+    pass
 
 
-class ValidationResultForm(WagtailAdminModelForm):
+class QAPackageForm(CoreAdminModelForm):
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data:
+            analyst = cleaned_data.get("analyst")
+            qa_decision = cleaned_data.get("qa_decision")
+            if not analyst and not qa_decision:
+                self.add_error(
+                    "qa_decision",
+                    _(
+                        "Inform your decision about the package or select an analyst to make the decision about the package"
+                    ),
+                )
+
     def save_all(self, user):
-        vr_obj = super().save(commit=False)
+        qa_package = super().save(commit=False)
+        if qa_package.qa_decision:
+            qa_package.status = qa_package.qa_decision
+            if qa_package.is_approved:
+                if not qa_package.approved_date:
+                    qa_package.approved_date = datetime.utcnow()
 
-        if self.instance.pk is None:
-            vr_obj.creator = user
+        if not qa_package.analyst:
+            qa_package.analyst = CollectionTeamMember.objects.get(user=user)
+        if qa_package.analyst:
+            qa_package.assignee = qa_package.analyst.user
+
+        qa_package.approved_date = qa_package.approved_date or datetime.utcnow()
 
         self.save()
 
-        return vr_obj
+        return qa_package
 
 
-class ValidationResultErrorResolutionForm(forms.Form):
-    validation_result_id = forms.IntegerField()
-    rationale = forms.CharField(widget=forms.Textarea, required=False)
-    action = forms.CharField(widget=forms.Select, required=False)
+class ApprovedPackageForm(CoreAdminModelForm):
+    pass
 
 
-class ValidationResultErrorResolutionOpinionForm(forms.Form):
-    validation_result_id = forms.IntegerField()
-    guidance = forms.CharField(widget=forms.Textarea, required=False)
-    opinion = forms.CharField(widget=forms.Select, required=False)
+class ValidationResultForm(CoreAdminModelForm):
+    pass
+
+
+class XMLErrorReportForm(CoreAdminModelForm):
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data:
+            if not cleaned_data.get("xml_producer_ack"):
+                self.add_error(
+                    "xml_producer_ack",
+                    _("Inform if you finish or not the errors review"),
+                )
+
+    def save_all(self, user):
+        obj = super().save(commit=False)
+        if obj.package.creator == obj.updated_by:
+            obj.package.save()
+
+        if obj.xml_producer_ack:
+            obj.conclusion = choices.REPORT_CREATION_DONE
+        self.save()
+
+        obj.package.calculate_validation_numbers()
+        return obj
+
+
+class UploadValidatorForm(CoreAdminModelForm):
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data:
+            max_x = cleaned_data.get("max_xml_errors_percentage")
+            if not max_x or not (0 <= max_x <= 100):
+                self.add_error(
+                    "max_xml_errors_percentage", _("Value must be from 0 to 100")
+                )
+
+            max_x = cleaned_data.get("max_impossible_to_fix_percentage")
+            if not max_x or not (0 <= max_x <= 100):
+                self.add_error(
+                    "max_impossible_to_fix_percentage", _("Value must be from 0 to 100")
+                )
