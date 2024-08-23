2618760461731a815c8aa6cd10fa53fdb6571b5e
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-08-22 21:24:26 -0300

    Cria rota para download_package

--- article/urls.py
+++ article/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+
+from article.views import download_package
+
 app_name = "article"
 
-urlpatterns = []
+urlpatterns = [
+    path("download-package", view=download_package, name="download_package"),
+]
