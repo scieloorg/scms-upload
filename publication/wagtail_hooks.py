30093e2ab7791aee482b042523541ad5a2f7d8b1
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-01-05 22:52:23 -0300

    Cria visualizacao para ArticleAvailabilityFileViewSet

--- publication/wagtail_hooks.py
+++ publication/wagtail_hooks.py
@@ -1,13 +1,13 @@
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
 
-from .models import ArticleAvailability
+from .models import ArticleAvailability, MissingArticle
 
 
-class ArticleAvailabilityViewSet(SnippetViewSet):
+class ArticleAvailabilitySiteViewSet(SnippetViewSet):
     model = ArticleAvailability
     menu_icon = "folder"
-    menu_order = 200
+    menu_order = 100
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = (
         False  # or True to exclude pages of this type from Wagtail's explorer view
@@ -19,4 +19,19 @@ class ArticleAvailabilityViewSet(SnippetViewSet):
     )
 
 
-register_snippet(ArticleAvailabilityViewSet)
+class ArticleAvailabilityFileViewSet(SnippetViewSet):
+    model = MissingArticle
+    menu_icon = "folder"
+    menu_order = 200
+    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
+    exclude_from_explorer = (
+        False  # or True to exclude pages of this type from Wagtail's explorer view
+    )
+    list_display = (
+        "get_collection_name",
+        "pid_v2",
+    )
+
+
+register_snippet(ArticleAvailabilitySiteViewSet)
+register_snippet(ArticleAvailabilityFileViewSet)
