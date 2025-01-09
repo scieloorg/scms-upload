cf37e5a6886bef61bf3dedf6dc6cc8808a1e99fd
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-01-09 11:47:53 -0300

    Remove visualizacao de MigratedArticle

--- publication/wagtail_hooks.py
+++ publication/wagtail_hooks.py
@@ -1,7 +1,7 @@
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
 
-from .models import ArticleAvailability, MissingArticle
+from .models import ArticleAvailability
 
 
 class ArticleAvailabilitySiteViewSet(SnippetViewSet):
@@ -18,20 +18,5 @@ class ArticleAvailabilitySiteViewSet(SnippetViewSet):
         "article__pid_v2",
     )
 
-
-class ArticleAvailabilityFileViewSet(SnippetViewSet):
-    model = MissingArticle
-    menu_icon = "folder"
-    menu_order = 200
-    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
-    exclude_from_explorer = (
-        False  # or True to exclude pages of this type from Wagtail's explorer view
-    )
-    list_display = (
-        "get_collection_name",
-        "pid_v2",
-    )
-
-
 register_snippet(ArticleAvailabilitySiteViewSet)
-register_snippet(ArticleAvailabilityFileViewSet)
+
