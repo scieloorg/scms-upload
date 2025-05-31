eaf6d44b6739764c3d3a4ff108735892aa4f9e4e
Roberta Takenaka <roberta.takenaka@scielo.org>
2025-05-31 17:45:24 -0300

    Refatora MigratedFile.find para ficar somente dependente do journal_acron, issue_folder e name encontrado no xlink_href

--- migration/models.py
+++ migration/models.py
@@ -459,31 +459,33 @@ class MigratedFile(CommonControlField):
         return obj
 
     @classmethod
-    def find(cls, collection, xlink_href, subdir):
-        path = MigratedFile.fix_asset_path(xlink_href, subdir)
+    def find(cls, collection, xlink_href, journal_acron):
+        try:
+            dirname = os.path.dirname(xlink_href)
+            basename = os.path.basename(xlink_href)
+            name, ext = os.path.splitext(basename)
 
-        name, ext = os.path.splitext(path)
+            issue_folder = dirname.split("/")[-1]
+            issue_folder__name = os.path.join(issue_folder, name)
 
-        return cls.objects.filter(
-            Q(original_href=path) | Q(original_href__startswith=name + "."),
-            collection=collection,
-        )
-
-    @staticmethod
-    def fix_asset_path(path, subdir):
-        for pattern in ("/img/revistas/", "/img/fbpe/"):
-            if path.startswith(pattern):
-                return path
-
-        for pattern in ("img/revistas/", "img/fbpe/"):
-            if pattern in path:
-                # faz path começar com /img/revistas
-                return "/" + path[path.find(pattern) :]
-
-        path = os.path.join("/img/revistas", subdir, path)
-        if ".." in path:
-            path = os.path.normpath(path)
-        return path
+            return cls.objects.filter(
+                Q(original_href__contains=issue_folder__name + "."),
+                collection=collection,
+                original_href__contains=f"/{journal_acron}/",
+            )
+        except Exception as e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            UnexpectedEvent.create(
+                e=e,
+                exc_traceback=exc_traceback,
+                detail={
+                    "task": "migrations.models.MigratedFile.find",
+                    "xlink_href": xlink_href,
+                    "journal_acron": journal_acron,
+                    "collection": str(collection),
+                },
+            )
+            return []
 
     def get_original_href(self, original_path):
         try:
