a3fff44e6d1839cc46f0fd1f0ce389c7701e01d5
Roberta Takenaka <roberta.takenaka@scielo.org>
2025-10-10 06:50:16 -0300

    feat(core): implementa CommonControlFieldViewSet com tracking de usuário
    - Adiciona UserTrackingCreateView e UserTrackingEditView
    - Implementa save_instance() para registrar creator e updated_by
    - Mantém compatibilidade com save_all() do CommonControlFieldCreateView
    - Adiciona logging para debug de usuário nas operações
    - Prepara base para migração de ModelAdmin para SnippetViewSet

--- core/views.py
+++ core/views.py
@@ -1,8 +1,48 @@
+import logging
+
 from django.http import HttpResponseRedirect
-from wagtail_modeladmin.views import CreateView
+
+from django.utils.translation import gettext_lazy as _
+from wagtail.snippets.views.snippets import CreateView, EditView, SnippetViewSet
+from wagtail.admin import messages
 
 
 class CommonControlFieldCreateView(CreateView):
     def form_valid(self, form):
         self.object = form.save_all(self.request.user)
         return HttpResponseRedirect(self.get_success_url())
+
+
+class CommonControlFieldViewSet(SnippetViewSet):
+    """
+    Mixin para adicionar tracking de usuário em qualquer SnippetViewSet
+    Compatível com Wagtail 6.4.2
+    """
+
+    class UserTrackingCreateView(CreateView):
+        def save_instance(self):
+            logging.info(f"User: {self.request.user}")
+            instance = self.form.save(commit=False)
+            if not instance.pk:
+                instance.creator = self.request.user
+            instance.updated_by = self.request.user
+            instance.save()
+            if hasattr(self.form, "save_m2m"):
+                self.form.save_m2m()
+            # self.log_action('wagtail.create')
+            return instance
+
+    class UserTrackingEditView(EditView):
+        def save_instance(self):
+            logging.info(f"User: {self.request.user}")
+            instance = self.form.save(commit=False)
+            instance.updated_by = self.request.user
+            instance.save()
+            if hasattr(self.form, "save_m2m"):
+                self.form.save_m2m()
+            # self.log_action('wagtail.edit')
+            return instance
+
+    # Define as views customizadas
+    add_view_class = UserTrackingCreateView
+    edit_view_class = UserTrackingEditView
