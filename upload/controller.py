ca381979ca5a66335d13eab68aea068e1e8d0f44
Roberta Takenaka <roberta.takenaka@scielo.org>
2026-02-18 18:30:00 -0300

    upload: refactor controller - replace legacy logic with receive_package, journal/issue checks and package validation flow

--- upload/controller.py
+++ upload/controller.py
@@ -1,39 +1,38 @@
+import os
 import logging
-from datetime import datetime
+import sys
+import traceback
 
-from article.controller import create_article
-from collection.models import WebSiteConfiguration
-from libs.dsm.publication.db import exceptions, mk_connection
-from package import choices as package_choices
-from package.models import SPSPkg
+from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
+from packtools.sps.models.dates import ArticleDates
+from packtools.sps.models.front_articlemeta_issue import ArticleMetaIssue
+from packtools.sps.models.journal_meta import ISSN, Title
+from packtools.sps.pid_provider.xml_sps_lib import GetXMLItemsError, XMLWithPre
 
-from .models import (
-    ErrorResolution,
-    ErrorResolutionOpinion,
+from article import choices as article_choices
+from article.models import Article
+from issue.models import Issue
+from journal.models import Journal
+from package.models import update_zip_file
+from pid_provider.requester import PidRequester
+from proc.controller import create_or_update_journal, create_or_update_issue
+
+from tracker.models import UnexpectedEvent
+from upload.models import (
     Package,
-    ValidationResult,
+    ValidationReport,
     choices,
 )
+from upload.utils import file_utils, xml_utils
+
+pp = PidRequester()
 
 
-def create_package(
-    article_id,
-    user_id,
-    file_name,
-    category=choices.PC_SYSTEM_GENERATED,
-    status=choices.PS_PUBLISHED,
-):
-    package = Package()
-    package.article_id = article_id
-    package.creator_id = user_id
-    package.created = datetime.utcnow()
-    package.file = file_name
-    package.category = category
-    package.status = status
+class UnexpectedPackageError(Exception): ...
 
-    package.save()
 
-    return package
+class PackageDataError(Exception): ...
 
 
 def get_last_package(article_id, **kwargs):
@@ -47,43 +46,322 @@ def get_last_package(article_id, **kwargs):
         return
 
 
-def establish_site_connection(url="scielo.br"):
+def receive_package(user, package):
     try:
-        host = WebSiteConfiguration.objects.get(url__icontains=url).db_uri
-    except WebSiteConfiguration.DoesNotExist:
-        return False
+        zip_xml_file_path = package.file.path
+        response = {}
+        for xml_with_pre in XMLWithPre.create(path=zip_xml_file_path):
 
+            # atualiza package name e linked
+            # TODO melhorar a forma de fazer link entre os artigos do pacote
+            if xml_with_pre.xmltree.find(".//related-article"):
+                for item in package.pkg_zip.packages.all():
+                    if item is not package:
+                        package.linked.add(item)
+            package.main_doi = xml_with_pre.main_doi
+            package.add_order(xml_with_pre.order, xml_with_pre.fpage)
+            package.save()
+
+            response = _check_article_and_journal(package, xml_with_pre, user=user)
+            logging.info(response)
+            update_zip_file(zip_xml_file_path, response, xml_with_pre)
+
+            package.article = response.get("article")
+            package.issue = (
+                response.get("issue") or package.article and package.article.issue
+            )
+            package.journal = (
+                response.get("journal") or package.article and package.article.journal
+            )
+            package.category = response.get("package_category")
+            package.status = response.get("package_status")
+            package.expiration_date = response.get("expiration_date")
+            package.save()
+
+            error = (
+                response.get("error_message")
+                or not package.journal
+                or not package.issue
+            )
+            if error:
+                report = ValidationReport.create_or_update(
+                    user=user,
+                    package=package,
+                    title=_("Package file"),
+                    category=choices.VAL_CAT_PACKAGE_FILE,
+                    reset_validations=True,
+                )
+                report.add_validation_result(
+                    status=response["error_level"],
+                    message=response["error_message"],
+                    data=str(response),
+                    subject=choices.VAL_CAT_PACKAGE_FILE,
+                )
+                # falhou, retorna response
+                report.finish_validations()
+                package.finish_reception(
+                    blocking_error_status=response.get("package_status")
+                )
+                return response
+
+            if package.article:
+                package.article.update_status()
+            return response
+    except GetXMLItemsError as exc:
+        # identifica os erros do arquivo Zip / XML
+        # TODO levar este código para o packtools / XMLWithPre
+        return _identify_file_error(package)
+
+
+def _identify_file_error(package):
+    # identifica os erros do arquivo Zip / XML
+    # TODO levar este código para o packtools / XMLWithPre
     try:
-        mk_connection(host=host)
-    except exceptions.DBConnectError:
-        return False
+        xml_path = None
+        xml_str = file_utils.get_xml_content_from_zip(package.file.path, xml_path)
+        xml_utils.get_etree_from_xml_content(xml_str)
+        return {}
+    except (
+        file_utils.BadPackageFileError,
+        file_utils.PackageWithoutXMLFileError,
+    ) as exc:
+        message = exc.message
+        data = None
 
-    return True
+    except xml_utils.XMLFormatError as e:
+        data = {
+            "column": e.column,
+            "row": e.start_row,
+            "snippet": xml_utils.get_snippet(xml_str, e.start_row, e.end_row),
+        }
+        message = e.message
+
+    report = ValidationReport.create_or_update(
+        package.creator,
+        package,
+        _("File Report"),
+        choices.VAL_CAT_PACKAGE_FILE,
+        reset_validations=True,
+    )
+    validation_result = report.add_validation_result(
+        status=choices.VALIDATION_RESULT_FAILURE,
+        message=message,
+        data=data,
+    )
+    return {"error_message": message, "error_level": choices.VALIDATION_RESULT_BLOCKING}
 
 
-def request_pid_for_accepted_packages(user):
-    # FIXME Usar package.SPSPkg no lugar de Package
-    for pkg in Package.objects.filter(
-        status=choices.PS_ACCEPTED, article__isnull=True
-    ).iterator():
-        # FIXME indicar se é atualização (True) ou novo (False)
-        is_published = None
+def _check_article_and_journal(package, xml_with_pre, user):
+    # verifica se o XML está registrado no sistema
+    response = {}
+    try:
+        response = pp.is_registered_xml_with_pre(xml_with_pre, xml_with_pre.filename)
+        logging.info(f"is_registered_xml_with_pre: {response}")
+        # verifica se o XML é esperado (novo, requer correção, requer atualização)
+        name, ext = os.path.splitext(xml_with_pre.filename)
+        _check_package_is_expected(response, package, name)
+        logging.info(f"_check_package_is_expected: {response}")
 
-        sps_pkg = SPSPkg.create_or_update(
-            user,
-            pkg.file.path,
-            package_choices.PKG_ORIGIN_INGRESS_WITH_VALIDATION,
-            reset_failures=True,
-            is_published=is_published,
+        # verifica se journal e issue estão registrados
+        xmltree = xml_with_pre.xmltree
+
+        _check_journal(response, xmltree, user)
+        logging.info(f"_check_journal: {response}")
+        _check_issue(response, xmltree, user)
+        logging.info(f"_check_issue: {response}")
+
+        # verifica a consistência dos dados de journal e issue
+        # no XML e na base de dados
+        _check_xml_and_registered_data_compability(response)
+        logging.info(f"_check_xml_and_registered_data_compability: {response}")
+
+        response["package_status"] = choices.PS_ENQUEUED_FOR_VALIDATION
+
+        _archive_pending_correction_package(response, name)
+        return response
+    except UnexpectedPackageError as e:
+        response["package_status"] = choices.PS_UNEXPECTED
+        response["error_message"] = str(e)
+        response["error_level"] = choices.VALIDATION_RESULT_BLOCKING
+        return response
+    except PackageDataError as e:
+        response["package_status"] = choices.PS_PENDING_CORRECTION
+        response["error_message"] = str(e)
+        response["error_level"] = choices.VALIDATION_RESULT_BLOCKING
+        return response
+    except Exception as e:
+        response["package_status"] = choices.PS_PENDING_CORRECTION
+        response["error_message"] = str(e)
+        response["error_level"] = choices.VALIDATION_RESULT_BLOCKING
+        return response
+
+
+def _check_package_is_expected(response, package, sps_pkg_name):
+    try:
+        article = Article.objects.get(pid_v3=response["v3"])
+        params = {"article": article}
+    except (Article.DoesNotExist, KeyError):
+        # artigo novo, inédito no sistema
+        article = None
+        params = {"name": sps_pkg_name}
+
+    logging.info(f"_check_package_is_expected: {params}")
+    try:
+        # se o pacote anterior está pendente de correção, então é aceitável
+        previous_package = Package.objects.filter(**params).order_by("-created")[1]
+    except IndexError:
+        previous_package = None
+
+    response["article"] = article
+
+    if previous_package and previous_package.status in choices.PS_WIP:
+        raise UnexpectedPackageError(
+            _(
+                "Not allowed to accept new package. There is a previous package which status={}. Search it and change the status to '{}'"
+            ).format(previous_package.status, choices.PS_PENDING_CORRECTION)
         )
 
-        response = create_article(user, sps_pkg)
+    if article:
+        if article.status == article_choices.AS_REQUIRE_UPDATE:
+            response["package_category"] = choices.PC_UPDATE
+        elif article.status == article_choices.AS_REQUIRE_ERRATUM:
+            response["package_category"] = choices.PC_ERRATUM
+        else:
+            response["package_category"] = choices.PC_UPDATE
+
+            raise UnexpectedPackageError(
+                _(
+                    "Not allowed to accept new package. There is a previous package which status={}. Search it and change the status to '{}'"
+                ).format(article.status, choices.PS_PENDING_CORRECTION)
+            )
+    else:
+        response["package_category"] = choices.PC_NEW_DOCUMENT
+        return
+
+
+def _archive_pending_correction_package(response, name):
+    params = {}
+    if response.get("article"):
+        params["article"] = response.get("article")
+    else:
+        params["name"] = name
+    Package.objects.filter(**params, status=choices.PS_PENDING_CORRECTION).update(
+        status=choices.PS_ARCHIVED
+    )
+
+
+def _check_journal(response, xmltree, user):
+    xml = Title(xmltree)
+    journal_title = xml.journal_title
+
+    xml = ISSN(xmltree)
+    issn_electronic = xml.epub
+    issn_print = xml.ppub
+
+    response["journal"] = create_or_update_journal(
+        journal_title, issn_electronic, issn_print, user
+    )
+
+    if not response["journal"]:
+        data = {
+            "journal_title": journal_title,
+            "issn_electronic": issn_electronic,
+            "issn_print": issn_print,
+        }
+        similar_journals = []
+        for j in Journal.get_similar_items(journal_title, issn_electronic, issn_print):
+            similar_journals.append(
+                {
+                    "journal_title": j.title,
+                    "issn_electronic": j.official_journal.issn_electronic,
+                    "issn_print": j.official_journal.issn_print,
+                }
+            )
+        if similar_journals:
+            similar_journals = _("Registered journals: {}. ").format(similar_journals)
+        else:
+            similar_journals = _("Found no registered journal. ")
+        raise PackageDataError(
+            _(
+                "Journal in XML must be a registered journal. Journal in XML: {}. {}. Register the journal on core.scielo.org"
+            ).format(data, similar_journals)
+        )
+
+
+def _check_issue(response, xmltree, user):
+    xml = ArticleDates(xmltree)
+    try:
+        publication_year = xml.collection_date["year"]
+    except (TypeError, KeyError, ValueError):
         try:
-            pkg.article = response["article"]
-            pkg.save()
-        except KeyError:
-            # TODO registrar em algum modelo os erros para que o usuário
-            # fique ciente de que houve erro
-            logging.exception(
-                f"Unable to create / update article {response['error_msg']}"
+            publication_year = xml.article_date["year"]
+        except (TypeError, KeyError, ValueError):
+            publication_year = None
+
+    xml = ArticleMetaIssue(xmltree)
+    response["issue"] = create_or_update_issue(
+        response["journal"], publication_year, xml.volume, xml.suppl, xml.number, user
+    )
+    logging.info(f"issue: {response['issue']}")
+    if not response["issue"]:
+        data = {
+            "journal": response["journal"],
+            "volume": xml.volume,
+            "number": xml.number,
+            "suppl": xml.suppl,
+            "publication_year": publication_year,
+        }
+        logging.info(f"_check_issue {data}")
+        items = None
+        if publication_year and xml.volume:
+            items = Issue.objects.filter(
+                Q(publication_year=publication_year) | Q(volume=xml.volume),
+                journal=response["journal"],
+            )
+        elif publication_year:
+            items = Issue.objects.filter(
+                Q(publication_year=publication_year), journal=response["journal"]
+            )
+        if not items or not items.count():
+            items = Issue.objects.filter(journal=response["journal"])
+
+        issues = []
+        for item in items.order_by("-publication_year"):
+            issues.append(
+                {
+                    "publication_year": publication_year,
+                    "volume": item.volume,
+                    "number": item.number,
+                    "supplement": item.supplement,
+                }
+            )
+        if issues:
+            issues = _("Registered issues: {}. ").format(issues)
+        else:
+            issues = _("{} has no registered issues").format(response["journal"])
+        raise PackageDataError(
+            _(
+                "Issue in XML must be a registered issue. Issue in XML {}. {}. Register the issue on core.scielo.org"
+            ).format(data, issues)
+        )
+
+
+def _check_xml_and_registered_data_compability(response):
+    article = response["article"]
+
+    if article:
+        journal = response["journal"]
+        if not journal == article.journal:
+            raise PackageDataError(
+                _("{} (registered, {}) differs from {} (XML, {})").format(
+                    article.journal, article.journal.id, journal, journal.id
+                )
+            )
+
+        issue = response["issue"]
+        if not issue == article.issue:
+            raise PackageDataError(
+                _("{} (registered, {}) differs from {} (XML, {})").format(
+                    article.issue, article.issue.id, issue, issue.id
+                )
             )
