b14f85bbe78bfd7629e78ad02a44d86c55f65f20
Patricia Morimoto <excermori@yahoo.com.br>
2026-03-11 17:04:06 -0300

    Melhorias no install.sh
    - Adiciona comentários
    - Executa alguns comandos para instalação ou atualização

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+#
+# Bash script for SciELO Upload Installation/Update
+#
+# How it works:
+#       ./install <Upload version>
+#
+#     Check the tag version at https://github.com/scieloorg/scms-upload/releases
 
-upload_version=$1
+
 if [ ! $1 ]; then
+    # Version is mandatory
     echo "Version not informed. Please, inform the Upload version. E.g.: v2.3.4, ./install.sh v2.3.4"
     exit 128
 fi
 
 UPLOAD_DIR=./upload
 ENV_FILES_DIR=.envs/.production
+IS_UPDATE=0
 
 echo "Checking directory $UPLOAD_DIR$ENV_FILES_DIR ..."
 
@@ -24,19 +33,26 @@ else
     echo "  Downloading files from Git repository ..."
     cd $UPLOAD_DIR
     wget -O Makefile https://raw.githubusercontent.com/scieloorg/scms-upload/refs/tags/$1/Makefile
-    #wget -O production.yml https://raw.githubusercontent.com/scieloorg/scms-upload/refs/tags/$1/production.yml
+    wget -O production.yml https://raw.githubusercontent.com/scieloorg/scms-upload/refs/tags/$1/production.yml
+    IS_UPDATE=1
 fi
-
 
 echo $1 > VERSION
 
 echo "Version $(cat VERSION) installed/updated!"
 
-read -p "Enter the Docker login user: " DOCKER_USER
-docker login -u $DOCKER_USER
+if [ $IS_UPDATE -eq 1 ]; then
+    # For installation: make Docker login and build DB
+    read -p "Enter the Docker login user: " DOCKER_USER
+    docker login -u $DOCKER_USER
+    make build compose=production.yml
+    make django_migrate compose=production.yml
+else
+    # For updating: update Django containers and make DB migrations
+    make update_webapp compose=production.yml
+    make django_migrate_fresh_migrations compose=production.yml
+fi
 
-make build compose=production.yml
-make django_migrate compose=production.yml
 make up compose=production.yml
 make ps compose=production.yml
 
