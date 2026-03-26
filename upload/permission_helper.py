c49ce09643dea1a5d7fd78d1809c7b1774b4ef5e
Roberta Takenaka <roberta.takenaka@scielo.org>
2026-03-26 08:25:09 -0300

    refactor(upload): migra UploadPermissionHelper para UploadPermissions
    - Herda de AppPermissions em vez de wagtail_modeladmin PermissionHelper
    - Converte métodos de instância para staticmethod/classmethod
    - Remove parâmetro obj das assinaturas
    - Adiciona user_can_access() e user_is_staff()

--- upload/permission_helper.py
+++ upload/permission_helper.py
@@ -1,8 +1,15 @@
-from wagtail_modeladmin.helpers import PermissionHelper
+"""
+Permissões específicas do módulo Upload.
 
+Herda de core.permissions.AppPermissions e define as regras de negócio
+próprias: quem pode acessar, quem é analista, permissões granulares.
+"""
+
+from core.permission_helper import AppPermissions
 from team.models import has_permission, CollectionTeamMember
 
 
+# Codenames de permissões específicas do Upload
 ACCESS_ALL_PACKAGES = "access_all_packages"
 ASSIGN_PACKAGE = "assign_package"
 FINISH_DEPOSIT = "finish_deposit"
@@ -10,34 +17,37 @@ SEND_VALIDATION_ERROR_RESOLUTION = "send_validation_error_resolution"
 ANALYSE_VALIDATION_ERROR_RESOLUTION = "analyse_validation_error_resolution"
 
 
-class UploadPermissionHelper(PermissionHelper):
-    def user_can_packagezip_create(self, user, obj):
-        return self.user_can_use_upload_module(user, obj)
+class UploadPermissions(AppPermissions):
+    app_label = "upload"
 
-    def user_can_use_upload_module(self, user, obj):
+    @staticmethod
+    def user_can_access(user):
+        """Verifica se o usuário tem acesso ao módulo Upload."""
         return has_permission(user)
 
-    def user_can_access_all_packages(self, user, obj):
-        return self.user_has_specific_permission(user, ACCESS_ALL_PACKAGES)
-
-    def user_can_assign_package(self, user, obj):
-        return self.user_has_specific_permission(user, ASSIGN_PACKAGE)
-
-    def user_can_finish_deposit(self, user, obj):
-        return self.user_has_specific_permission(user, FINISH_DEPOSIT)
-
-    def user_can_send_error_validation_resolution(self, user, obj):
-        return self.user_has_specific_permission(user, SEND_VALIDATION_ERROR_RESOLUTION)
-
-    def user_can_analyse_error_validation_resolution(self, user, obj):
-        return self.user_has_specific_permission(
-            user, ANALYSE_VALIDATION_ERROR_RESOLUTION
-        )
-
-    def user_is_analyst_team_member(self, user, obj):
-        if self.user_can_use_upload_module(user, obj):
+    @staticmethod
+    def user_is_staff(user):
+        """Verifica se o usuário é membro de equipe analista."""
+        if UploadPermissions.user_can_access(user):
             return CollectionTeamMember.objects.filter(user=user).exists()
+        return False
 
-    def user_is_xml_producer(self, user, obj):
-        if self.user_can_use_upload_module(user, obj):
-            return not self.user_is_analyst_team_member(user, obj)
+    @classmethod
+    def user_can_access_all_packages(cls, user):
+        return cls.user_has_permission(user, ACCESS_ALL_PACKAGES)
+
+    @classmethod
+    def user_can_assign_package(cls, user):
+        return cls.user_has_permission(user, ASSIGN_PACKAGE)
+
+    @classmethod
+    def user_can_finish_deposit(cls, user):
+        return cls.user_has_permission(user, FINISH_DEPOSIT)
+
+    @classmethod
+    def user_can_send_error_validation_resolution(cls, user):
+        return cls.user_has_permission(user, SEND_VALIDATION_ERROR_RESOLUTION)
+
+    @classmethod
+    def user_can_analyse_error_validation_resolution(cls, user):
+        return cls.user_has_permission(user, ANALYSE_VALIDATION_ERROR_RESOLUTION)
