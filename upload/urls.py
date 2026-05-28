93cf6f08d972012a4863528e99e2d2a437093824
Roberta Takenaka <roberta.takenaka@scielo.org>
2026-05-28 18:48:21 -0300

    feat(upload): registra rota batch-republish nas URLs do app

--- upload/urls.py
+++ upload/urls.py
@@ -7,6 +7,7 @@ from .views import (
     finish_deposit,
     preview_document,
     archive_package,
+    batch_republish,
 )
 
 app_name = "upload"
@@ -18,4 +19,5 @@ urlpatterns = [
     path("download-errors", view=download_errors, name="download_errors"),
     path("xml", view=display_xml, name="display_xml"),
     path("archive", view=archive_package, name="archive_package"),
+    path("batch-republish", view=batch_republish, name="batch_republish"),
 ]
