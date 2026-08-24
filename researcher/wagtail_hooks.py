9d95036454c5e72e22ea7f845e03db17788c196f
Rafael JPD <rafael@pitangainnovare.com.br>
2026-08-24 17:04:34 -0300

    Restringe entidades editoriais de referência
    Implementação: aplica a política compartilhada a instituições, localizações e pesquisadores.

--- researcher/wagtail_hooks.py
+++ researcher/wagtail_hooks.py
@@ -3,12 +3,14 @@ from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
 
 from config.menu import get_menu_order
+from core.users.permission_policies import TeamScopedSnippetViewSetMixin
 
 from .models import Researcher
 
 
-class ResearcherViewSet(SnippetViewSet):
+class ResearcherViewSet(TeamScopedSnippetViewSetMixin, SnippetViewSet):
     model = Researcher
+    allow_unscoped_queryset = True
     menu_label = _("Researcher")
     menu_icon = "folder"
     menu_order = get_menu_order("researcher")
