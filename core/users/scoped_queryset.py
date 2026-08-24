69e3afb8feb6c6c9da8b0906c23d250a81a6f8a8
Rafael JPD <rafael@pitangainnovare.com.br>
2026-08-24 16:55:13 -0300

    Introduz o escopo relacional por membresia
    Implementação: filtra querysets por coleção, periódico ou empresa conforme os vínculos ativos do usuário.

---
+++ core/users/scoped_queryset.py
@@ -0,0 +1,40 @@
+from django.db.models import Q
+
+from team.models import get_user_membership_ids
+
+
+def scope_by_membership(
+    user,
+    queryset,
+    collection_field=None,
+    journal_field=None,
+    company_field=None,
+):
+    if user and user.is_superuser:
+        return queryset
+    if not user or not user.is_authenticated:
+        return queryset.none()
+
+    membership = get_user_membership_ids(user)
+    collection_ids = membership.get("collection_list_ids") or []
+    journal_ids = membership.get("journal_list_ids") or []
+    company_ids = membership.get("company_list_ids") or []
+
+    if not (collection_ids or journal_ids or company_ids):
+        return queryset.none()
+
+    query = Q()
+
+    if collection_field and collection_ids:
+        query |= Q(**{f"{collection_field}__in": collection_ids})
+
+    if journal_field and journal_ids:
+        query |= Q(**{f"{journal_field}__in": journal_ids})
+
+    if company_field and company_ids:
+        query |= Q(**{f"{company_field}__in": company_ids})
+
+    if not query:
+        return queryset.none()
+
+    return queryset.filter(query)
